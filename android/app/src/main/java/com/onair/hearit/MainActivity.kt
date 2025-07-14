5f2f0505fd637d6e527f9eb99f8fd1ace56abcc8
gahyunkim <mapledt001@naver.com>
2025-07-14 16:02:18 +0900

    [AN] 바텀 내비게이션 view 추가 #11
    * [AN] build: databinding 추가 #11
    
    * [AN] feat: Bottom Navigation View 추가 #11

--- android/app/src/main/java/com/onair/hearit/MainActivity.kt
+++ android/app/src/main/java/com/onair/hearit/MainActivity.kt
@@ -5,16 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.onair.hearit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
+
+        binding.layoutBottomNavigation.itemIconTintList = null
     }
 }
