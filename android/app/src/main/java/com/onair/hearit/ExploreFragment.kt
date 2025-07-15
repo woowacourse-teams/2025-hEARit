50babdd6c607a0d4df328ea35a5f2b34d8f7c6c6
gahyunkim <mapledt001@naver.com>
2025-07-15 15:21:49 +0900

    [AN] fix: ExploreFragment 상단 inset 추가 #13

--- android/app/src/main/java/com/onair/hearit/ExploreFragment.kt
+++ android/app/src/main/java/com/onair/hearit/ExploreFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
@@ -37,6 +39,12 @@ class ExploreFragment : Fragment() {
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(0, systemBars.top, 0, 0)
+            insets
+        }
 
         setupRecyclerView()
     }
