9ae912d952b365c8713ae010c611889f0643641b
gahyunkim <mapledt001@naver.com>
2025-07-15 15:26:47 +0900

    [AN] chore: Conflict 수정 #13

--- android/app/src/main/java/com/onair/hearit/BaseControllerView.kt
+++ android/app/src/main/java/com/onair/hearit/BaseControllerView.kt
@@ -114,9 +114,9 @@ class BaseControllerView
 
         private fun updatePlayPauseButton() {
             if (player.playWhenReady && player.playbackState == Player.STATE_READY) {
-                playButton.setImageResource(R.drawable.ic_pause1)
+                playButton.setImageResource(R.drawable.ic_pause)
             } else {
-                playButton.setImageResource(R.drawable.ic_play1)
+                playButton.setImageResource(R.drawable.ic_play)
             }
         }
 
