b33236303af8a040f1857aa4a4604207e6fe72e9
Ham BeomJoon <hbj0802@naver.com>
2025-07-15 19:28:56 +0900

    [AN] build: firebase plugin 추가 #18

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -57,4 +58,8 @@ dependencies {
 
     // lottie
     implementation(libs.lottie)
+
+    // firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 }
