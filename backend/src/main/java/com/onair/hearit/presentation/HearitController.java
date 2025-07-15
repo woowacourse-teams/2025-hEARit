22181a9f52cb0ca859eca9c79a27cf7ef9483849
JO-eusan <goodsan0330@gmail.com>
2025-07-15 21:12:58 +0900

    [BE] feat: /random 요청 시 랜덤 히어릿 조회 기능 호출 #27
    - 정상 호출 시 200 OK

---
+++ backend/src/main/java/com/onair/hearit/presentation/HearitController.java
@@ -0,0 +1,23 @@
+package com.onair.hearit.presentation;
+
+import com.onair.hearit.application.HearitService;
+import com.onair.hearit.dto.response.HearitExploreResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/hearit")
+public class HearitController {
+
+    private final HearitService hearitService;
+
+    @GetMapping("/random")
+    public ResponseEntity<List<HearitExploreResponse>> readExploredHearits() {
+        return ResponseEntity.ok(hearitService.getExploredHearits());
+    }
+}
