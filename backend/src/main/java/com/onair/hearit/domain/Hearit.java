f932069d5b8c4f2487f531a30966e478cd985662
gabean13 <choegabin171@gmail.com>
2025-07-15 14:22:38 +0900

    [BE] chore: entity 추가 #20
    - Bookmark, Category, Hearit, HearitKeyword, Keyword, Member 엔티티 생성

---
+++ backend/src/main/java/com/onair/hearit/domain/Hearit.java
@@ -0,0 +1,53 @@
+package com.onair.hearit.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Table(name = "hearit")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Hearit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "summary")
+    private String summary;
+
+    @Column(name = "play_time", nullable = false)
+    private Integer playTime;
+
+    @Column(name = "audio_url", nullable = false)
+    private String audioUrl;
+
+    @Column(name = "script_url", nullable = false)
+    private String scriptUrl;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+}
