91df2eecf63c801859717b005e9b3fec64b4ae88
JO-eusan <goodsan0330@gmail.com>
2025-07-16 11:28:19 +0900

    [BE] chore: 개발/테스트 환경 프로퍼티 설정 업데이트
    - 테스트 코드에 ActiveProfiles 적용

--- backend/src/test/java/com/onair/hearit/infrastructure/HearitRepositoryTest.java
+++ backend/src/test/java/com/onair/hearit/infrastructure/HearitRepositoryTest.java
@@ -13,8 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@ActiveProfiles("fake-test")
 class HearitRepositoryTest {
 
     @Autowired
