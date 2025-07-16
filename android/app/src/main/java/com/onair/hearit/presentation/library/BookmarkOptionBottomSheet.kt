36ddc612efa366244e6fd14b4aadab27d58b909e
rosemin928 <rosemin928@naver.com>
2025-07-16 16:57:08 +0900

    [AN] refactor: 북마크 옵션 BottomSheet 스타일 변경 #29

--- android/app/src/main/java/com/onair/hearit/presentation/library/BookmarkOptionBottomSheet.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/library/BookmarkOptionBottomSheet.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.onair.hearit.databinding.BottomSheetBookmarkOptionBinding
 
-class BookmarkOptionBottomSheet : RoundedBottomSheetDialogFragment() {
+class BookmarkOptionBottomSheet : BottomSheetDialogFragment() {
     @Suppress("ktlint:standard:backing-property-naming")
     private var _binding: BottomSheetBookmarkOptionBinding? = null
     private val binding get() = _binding!!
