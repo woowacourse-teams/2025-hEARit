051cef24c88bfe615dba41f624e821c51dc45b86
YuyoungRhee <bbclara@naver.com>
2025-07-18 17:56:34 +0900

    [BE] docs: swagger 문서에 서버 설정 추가(로컬, 운영 구분) #78

--- backend/src/main/java/com/onair/hearit/common/config/SwaggerConfig.java
+++ backend/src/main/java/com/onair/hearit/common/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,6 +25,10 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Hearit API")
                         .description("히어잇 백엔드 API 문서")
-                        .version("v1"));
+                        .version("v1"))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("로컬 서버"),
+                        new Server().url("http://hearit.o-r.kr").description("운영 서버")
+                ));
     }
 }
