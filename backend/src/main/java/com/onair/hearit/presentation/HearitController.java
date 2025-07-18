6026f7c0c288d50610f3b2a9e90ef6131ec3e4f0
Gabin Choi <choegabin171@gmail.com>
2025-07-18 15:50:20 +0900

    [BE] feat: 실제 멤버 변수로 수정 #49

--- backend/src/main/java/com/onair/hearit/presentation/HearitController.java
+++ backend/src/main/java/com/onair/hearit/presentation/HearitController.java
@@ -1,10 +1,11 @@
 package com.onair.hearit.presentation;
 
 import com.onair.hearit.application.HearitService;
-import com.onair.hearit.dto.response.HearitDetailResponse;
+import com.onair.hearit.auth.dto.CurrentMember;
 import com.onair.hearit.dto.response.HearitPersonalDetailResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +18,10 @@ public class HearitController {
 
     private final HearitService hearitService;
 
-    //TODO 실제 멤버로 변경
     @GetMapping("/{hearitId}")
-    public ResponseEntity<HearitPersonalDetailResponse> readHearit(@PathVariable Long hearitId, Long memberId) {
-        HearitPersonalDetailResponse response = hearitService.getHearitPersonalDetail(hearitId, memberId);
+    public ResponseEntity<HearitPersonalDetailResponse> readHearit(@PathVariable Long hearitId,
+                                                                   @AuthenticationPrincipal CurrentMember member) {
+        HearitPersonalDetailResponse response = hearitService.getHearitPersonalDetail(hearitId, member.memberId());
         return ResponseEntity.ok(response);
     }
 }
