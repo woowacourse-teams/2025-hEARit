7e64fa09649c248739ae3021f7a73952963af989
YuyoungRhee <bbclara@naver.com>
2025-07-18 17:34:23 +0900

    [BE] chore: Swagger UI를 위한 초기 설정 세팅 #78
    - swagger 의존성 추가
    - SwaggerConfig으로 공통 설정
    - SecurityConfig에서 swagger 관련한 모든 url permit
    - application-dev.properties에 swagger 설정 추

---
+++ backend/src/main/java/com/onair/hearit/common/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.onair.hearit.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .info(new Info()
+                        .title("Hearit API")
+                        .description("히어잇 백엔드 API 문서")
+                        .version("v1"));
+    }
+}
