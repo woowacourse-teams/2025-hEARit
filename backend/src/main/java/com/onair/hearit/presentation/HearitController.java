a282d256787477774e5bd3143866ab158fb2d4fe
YuyoungRhee <bbclara@naver.com>
2025-07-18 17:35:20 +0900

    [BE] docs: Controller에 설명 추가하여 문서화 #78
    - Swagger 어노테이션을 통해 Tag, summary, description 등 설정

--- backend/src/main/java/com/onair/hearit/presentation/HearitController.java
+++ backend/src/main/java/com/onair/hearit/presentation/HearitController.java
@@ -2,6 +2,8 @@ package com.onair.hearit.presentation;
 
 import com.onair.hearit.application.HearitService;
 import com.onair.hearit.dto.response.HearitDetailResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/hearits")
+@Tag(name = "Hearit", description = "히어릿")
 public class HearitController {
 
     private final HearitService hearitService;
 
+    @Operation(summary = "단일 히어릿 상세 조회", description = "히어릿ID룰 통해 하나의 히어릿 상세 정보를 조회합니다.")
     @GetMapping("/{hearitId}")
     public ResponseEntity<HearitDetailResponse> readHearit(@PathVariable Long hearitId) {
         HearitDetailResponse response = hearitService.getHearitDetail(hearitId);
