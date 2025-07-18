31da9f6bbb586fde099800bbcc0be9ed9b6e7128
의엉 <127360730+YuyoungRhee@users.noreply.github.com>
2025-07-18 18:03:54 +0900

    [BE] Swagger UI 설정 및 문서 기본 설명 추가 #78
    * [BE] chore: Swagger UI를 위한 초기 설정 세팅 #78
    
    - swagger 의존성 추가
    - SwaggerConfig으로 공통 설정
    - SecurityConfig에서 swagger 관련한 모든 url permit
    - application-dev.properties에 swagger 설정 추
    
    * [BE] docs: Controller에 설명 추가하여 문서화 #78
    
    - Swagger 어노테이션을 통해 Tag, summary, description 등 설정
    
    * [BE] docs: swagger 문서에 서버 설정 추가(로컬, 운영 구분) #78

--- backend/src/main/java/com/onair/hearit/presentation/FileSourceController.java
+++ backend/src/main/java/com/onair/hearit/presentation/FileSourceController.java
@@ -4,6 +4,8 @@ import com.onair.hearit.application.FileSourceService;
 import com.onair.hearit.dto.response.OriginalAudioResponse;
 import com.onair.hearit.dto.response.ScriptResponse;
 import com.onair.hearit.dto.response.ShortAudioResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,22 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/hearits")
+@Tag(name = "FileSource", description = "오디오 및 스크립트 파일")
 public class FileSourceController {
 
     private final FileSourceService fileSourceService;
 
+    @Operation(summary = "원본 오디오 파일 조회", description = "히어릿ID룰 통해 원본 오디오 파일을 조회합니다.")
     @GetMapping("/{hearitId}/original-audio-url")
     public ResponseEntity<OriginalAudioResponse> readOriginalAudioUrl(@PathVariable Long hearitId) {
         OriginalAudioResponse response = fileSourceService.getOriginalAudio(hearitId);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "1분 미리보기 오디오 파일 조회", description = "히어릿ID룰 통해 1분 미리보기 오디오 파일을 조회합니다.")
     @GetMapping("/{hearitId}/short-audio-url")
     public ResponseEntity<ShortAudioResponse> readShortAudioUrl(@PathVariable Long hearitId) {
         ShortAudioResponse response = fileSourceService.getShortAudio(hearitId);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "스크립트 파일 조회", description = "히어릿ID룰 통해 스크립트 파일을 조회합니다.")
     @GetMapping("/{hearitId}/script-url")
     public ResponseEntity<ScriptResponse> readScriptUrl(@PathVariable Long hearitId) {
         ScriptResponse response = fileSourceService.getScript(hearitId);
