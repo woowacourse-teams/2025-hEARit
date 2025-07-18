29cf4de9cbc23e909d6e4b36915a86dd048c8378
San <goodsan0330@gmail.com>
2025-07-18 17:36:30 +0900

    [BE] 랜덤/추천 히어릿 리스트 조회 API #44
    * [BE] feat: 탐색 탭을 위한 랜덤 히어릿 10개 검색 로직 구현 #44
    
    - Pageable을 활용해 구현
    - 랜덤 셔플은 Repository에서 진행 (MySQL, H2 집계 함수)
    
    * [BE] feat: /hearits/random API 연결 #44
    
    - HearitService의 getExploredHearits()와 연결
    
    * [BE] test: 탐색 탭에서의 랜덤 히어릿 조회 성공 테스트 #44
    
    * [BE] feat: 홈 화면에서의 추천 히어릿 및 랜덤 히어릿 로직 구현 #44
    
    - 추천 히어릿은 1번 인덱스로 고정
    - 나머지 4개의 히어릿은 랜덤
    
    * [BE] test: 추천 히어릿 탐색 테스트 #44
    
    * [BE] test: /hearits/recommend API 연결 #44
    
    - 추천 히어릿을 랜덤 방식과 동일하게 변경
    - HearitService의 getRecommendedHearits()와 연결
    
    * [BE] fix: auth 한글 테스트명 영어로 변경 #44
    
    * [BE] style: 테스트 띄어쓰기, public 키워드 제거 #44
    
    * [BE] style: Controller 테스트 이름 변경 #44

--- backend/src/main/java/com/onair/hearit/presentation/HearitController.java
+++ backend/src/main/java/com/onair/hearit/presentation/HearitController.java
@@ -2,6 +2,7 @@ package com.onair.hearit.presentation;
 
 import com.onair.hearit.application.HearitService;
 import com.onair.hearit.dto.response.HearitDetailResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,5 +21,17 @@ public class HearitController {
     public ResponseEntity<HearitDetailResponse> readHearit(@PathVariable Long hearitId) {
         HearitDetailResponse response = hearitService.getHearitDetail(hearitId);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/random")
+    public ResponseEntity<List<HearitDetailResponse>> readRandomHearits() {
+        List<HearitDetailResponse> responses = hearitService.getRandomHearits();
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/recommend")
+    public ResponseEntity<List<HearitDetailResponse>> readRecommendedHearits() {
+        List<HearitDetailResponse> responses = hearitService.getRecommendedHearits();
+        return ResponseEntity.ok(responses);
     }
 }
