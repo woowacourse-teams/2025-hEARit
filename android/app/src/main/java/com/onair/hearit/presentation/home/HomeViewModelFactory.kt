dc19f418300e5dcde13bc1179c6b43f2ea430760
Ham BeomJoon <hbj0802@naver.com>
2025-07-21 19:21:20 +0900

    [AN] chore: casting Suppress 추가 #93

--- android/app/src/main/java/com/onair/hearit/presentation/home/HomeViewModelFactory.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/home/HomeViewModelFactory.kt
@@ -7,6 +7,7 @@ import com.onair.hearit.data.HearitRemoteDataSourceImpl
 import com.onair.hearit.data.HearitRepositoryImpl
 import com.onair.hearit.di.NetworkProvider
 
+@Suppress("UNCHECKED_CAST")
 class HomeViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(
         modelClass: Class<T>,
