b840a5efc3a170f9fbd55444f4d963353571a860
gahyunkim <mapledt001@naver.com>
2025-07-22 15:41:14 +0900

    [AN] feat: SearchHearitResponse data class 추가 및 toDomain 매퍼 함수 추가 #108

---
+++ android/app/src/main/java/com/onair/hearit/data/SearchHearitResponse.kt
@@ -0,0 +1,25 @@
+package com.onair.hearit.data
+
+import com.onair.hearit.domain.SearchedHearitItem
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SearchHearitResponse(
+    @SerialName("id")
+    val id: Long,
+    @SerialName("playTime")
+    val playTime: Int,
+    @SerialName("summary")
+    val summary: String,
+    @SerialName("title")
+    val title: String,
+)
+
+fun SearchHearitResponse.toDomain(): SearchedHearitItem =
+    SearchedHearitItem(
+        id = this.id,
+        title = this.title,
+        playTime = this.playTime,
+        summary = this.summary,
+    )
