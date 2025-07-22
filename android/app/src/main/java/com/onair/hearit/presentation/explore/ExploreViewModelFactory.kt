9f02f8fb66d2a3c01f34419bfca69bbd02a87ddd
gahyunkim <mapledt001@naver.com>
2025-07-22 15:48:18 +0900

    [AN] refactor: ExploreViewModelFactory에서 불필요한 extras 파라미터 제거 #101

--- android/app/src/main/java/com/onair/hearit/presentation/explore/ExploreViewModelFactory.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/explore/ExploreViewModelFactory.kt
@@ -2,7 +2,6 @@ package com.onair.hearit.presentation.explore
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.CreationExtras
 import com.onair.hearit.data.HearitRemoteDataSourceImpl
 import com.onair.hearit.data.HearitRepositoryImpl
 import com.onair.hearit.data.MediaFileRemoteDataSourceImpl
@@ -11,10 +10,7 @@ import com.onair.hearit.di.NetworkProvider
 
 @Suppress("UNCHECKED_CAST")
 class ExploreViewModelFactory : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(
-        modelClass: Class<T>,
-        extras: CreationExtras,
-    ): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         val hearitRemoteDataSource = HearitRemoteDataSourceImpl(NetworkProvider.hearitService)
         val hearitRepository = HearitRepositoryImpl(hearitRemoteDataSource)
         val mediaFileRemoteDataSource =
