63e907e54b72ce9ee976e368903a3f90420f8dfb
byesol <bsj5967@naver.com>
2025-07-22 17:20:59 +0900

    [BE] refactor: Invalid 페이지 예외처리 # 83

--- backend/src/main/java/com/onair/hearit/dto/request/KeywordSearchCondition.java
+++ backend/src/main/java/com/onair/hearit/dto/request/KeywordSearchCondition.java
@@ -1,6 +1,7 @@
 package com.onair.hearit.dto.request;
 
 import com.onair.hearit.common.exception.custom.InvalidInputException;
+import com.onair.hearit.common.exception.custom.InvalidPageException;
 
 public record KeywordSearchCondition(
         Long keywordId,
@@ -12,10 +13,10 @@ public record KeywordSearchCondition(
             throw new InvalidInputException("키워드 id는 null이 될 수 없습니다. ");
         }
         if (page < 0) {
-            throw new InvalidInputException("page는 0 이상의 값이어야 합니다.");
+            throw new InvalidPageException(0);
         }
         if (size < 0 || size > 50) {
-            throw new InvalidInputException("size는 0 이상 50 이하의 값이어야 합니다.");
+            throw new InvalidPageException(0, 50);
         }
     }
 }
