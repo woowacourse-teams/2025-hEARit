287fe9b3f75e3b8edbe465947f77f287bb54cb4d
YuyoungRhee <bbclara@naver.com>
2025-07-23 10:42:48 +0900

    [BE] fix: cors로 인한 개발서버 swagger api 테스트 실패 해결  #119

--- backend/src/main/java/com/onair/hearit/common/config/SwaggerConfig.java
+++ backend/src/main/java/com/onair/hearit/common/config/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig {
                         .description("히어릿 백엔드 API 문서")
                         .version("v1"))
                 .servers(List.of(
-                        new Server().url("http://localhost:8080").description("로컬 서버"),
+                        new Server().url("http://localhost:80").description("로컬 서버"),
                         new Server().url("http://hearit.o-r.kr").description("운영 서버")
                 ));
     }
