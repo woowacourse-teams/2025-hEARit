4fc432fd45e95e93c1759621fd3806caa804eff8
gahyunkim <mapledt001@naver.com>
2025-07-23 14:58:36 +0900

    [AN] refactor: MediaFileRepository에서 getShortsHearitItem 메서드 삭제 #116

--- android/app/src/main/java/com/onair/hearit/data/repository/MediaFileRepositoryImpl.kt
+++ android/app/src/main/java/com/onair/hearit/data/repository/MediaFileRepositoryImpl.kt
@@ -1,12 +1,9 @@
 package com.onair.hearit.data.repository
 
 import com.onair.hearit.data.datasource.MediaFileRemoteDataSource
-import com.onair.hearit.domain.HearitShorts
-import com.onair.hearit.domain.RandomHearitItem
 import com.onair.hearit.domain.ScriptLine
 import com.onair.hearit.domain.ShortAudioUrl
 import com.onair.hearit.domain.repository.MediaFileRepository
-import com.onair.hearit.domain.toHearitShortsItem
 import kotlinx.serialization.json.Json
 
 class MediaFileRepositoryImpl(
@@ -24,35 +21,10 @@ class MediaFileRepositoryImpl(
     override suspend fun getSubtitleLines(hearitId: Long): Result<List<ScriptLine>> =
         handleResult {
             val scriptUrl =
-                mediaFileRemoteDataSource
-                    .getScriptUrl(hearitId)
-                    .getOrElse { throw it }
-                    .url
-
+                mediaFileRemoteDataSource.getScriptUrl(hearitId).getOrElse { throw it }.url
             val responseBody =
-                mediaFileRemoteDataSource
-                    .getScriptJson(scriptUrl)
-                    .getOrElse { throw it }
-
+                mediaFileRemoteDataSource.getScriptJson(scriptUrl).getOrElse { throw it }
             val jsonString = responseBody.string()
-            Json.Default.decodeFromString(jsonString)
-        }
-
-    override suspend fun getShortsHearitItem(item: RandomHearitItem): Result<HearitShorts> = combine(item)
-
-    private suspend fun combine(item: RandomHearitItem): Result<HearitShorts> =
-        handleResult {
-            val hearitId = item.id
-
-            val audioUrl =
-                getShortAudioUrl(hearitId)
-                    .getOrElse { throw it }
-                    .url
-
-            val scriptList =
-                getSubtitleLines(hearitId)
-                    .getOrElse { throw it }
-
-            item.toHearitShortsItem(audioUrl, scriptList)
+            Json.decodeFromString(jsonString)
         }
 }
--- android/app/src/main/java/com/onair/hearit/domain/repository/MediaFileRepository.kt
+++ android/app/src/main/java/com/onair/hearit/domain/repository/MediaFileRepository.kt
@@ -1,7 +1,5 @@
 package com.onair.hearit.domain.repository
 
-import com.onair.hearit.domain.HearitShorts
-import com.onair.hearit.domain.RandomHearitItem
 import com.onair.hearit.domain.ScriptLine
 import com.onair.hearit.domain.ShortAudioUrl
 
@@ -9,6 +7,4 @@ interface MediaFileRepository {
     suspend fun getShortAudioUrl(hearitId: Long): Result<ShortAudioUrl>
 
     suspend fun getSubtitleLines(hearitId: Long): Result<List<ScriptLine>>
-
-    suspend fun getShortsHearitItem(item: RandomHearitItem): Result<HearitShorts>
 }
