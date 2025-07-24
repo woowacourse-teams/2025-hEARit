e8f478c1703e1e31f595825d47f077055a169013
gahyunkim <mapledt001@naver.com>
2025-07-24 13:24:03 +0900

    [AN] refactor: Hearit 검색 응답 DTO 수정 및 Paging 모델 추가 #143

--- android/app/src/main/java/com/onair/hearit/data/dto/SearchHearitResponse.kt
+++ android/app/src/main/java/com/onair/hearit/data/dto/SearchHearitResponse.kt
@@ -5,12 +5,30 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SearchHearitResponse(
-    @SerialName("id")
-    val id: Long,
-    @SerialName("playTime")
-    val playTime: Int,
-    @SerialName("summary")
-    val summary: String,
-    @SerialName("title")
-    val title: String,
-)
+    @SerialName("content")
+    val content: List<Content>,
+    @SerialName("page")
+    val page: Int,
+    @SerialName("size")
+    val size: Int,
+    @SerialName("totalPages")
+    val totalPages: Int,
+    @SerialName("totalElements")
+    val totalElements: Int,
+    @SerialName("isFirst")
+    val isFirst: Boolean,
+    @SerialName("isLast")
+    val isLast: Boolean,
+) {
+    @Serializable
+    data class Content(
+        @SerialName("id")
+        val id: Long,
+        @SerialName("playTime")
+        val playTime: Int,
+        @SerialName("summary")
+        val summary: String,
+        @SerialName("title")
+        val title: String,
+    )
+}
