ad1a5025c6c561b2b1058199abca2554b24242e5
rosemin928 <rosemin928@naver.com>
2025-07-24 13:30:00 +0900

    [AN] refactor: 사용자 정보 조회 Authorization 추가 #125

--- android/app/src/main/java/com/onair/hearit/data/datasource/MemberRemoteDataSourceImpl.kt
+++ android/app/src/main/java/com/onair/hearit/data/datasource/MemberRemoteDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.onair.hearit.data.datasource
 
 import com.onair.hearit.data.api.MemberService
 import com.onair.hearit.data.dto.UserInfoResponse
+import com.onair.hearit.di.TokenProvider
 import com.onair.hearit.domain.UserNotRegisteredException
 
 class MemberRemoteDataSourceImpl(
@@ -9,11 +10,27 @@ class MemberRemoteDataSourceImpl(
 ) : MemberRemoteDataSource {
     override suspend fun getUserInfo(): Result<UserInfoResponse> =
         runCatching {
-            val response = memberService.getUserInfo()
+            val response = memberService.getUserInfo(getAuthHeader())
             when (response.code()) {
-                200 -> response.body() ?: throw IllegalStateException("응답 바디가 null입니다.")
-                401 -> throw UserNotRegisteredException("회원가입되지 않은 사용자입니다.")
+                200 ->
+                    response.body() ?: throw IllegalStateException(
+                        ERROR_RESPONSE_BODY_NULL_MESSAGE,
+                    )
+
+                401 -> throw UserNotRegisteredException(ERROR_SIGN_IN_MESSAGE)
                 else -> throw Exception("API 호출 실패: ${response.code()} ${response.message()}")
             }
         }
+
+    private fun getAuthHeader(): String {
+        val token = requireNotNull(TokenProvider.accessToken) { ERROR_TOKEN_NOT_FOUND_MESSAGE }
+        return TOKEN.format(token)
+    }
+
+    companion object {
+        private const val ERROR_SIGN_IN_MESSAGE = "회원가입되지 않은 사용자입니다."
+        private const val ERROR_RESPONSE_BODY_NULL_MESSAGE = "응답 바디가 null입니다."
+        private const val ERROR_TOKEN_NOT_FOUND_MESSAGE = "토큰이 존재하지 않음"
+        private const val TOKEN = "Bearer %s"
+    }
 }
