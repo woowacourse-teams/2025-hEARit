d4b1a7433129e5573b064972ecfd0695cd7af28d
Ham BeomJoon <hbj0802@naver.com>
2025-07-24 20:37:53 +0900

    [AN] refactor: 카테고리 목록 조회 응답 페이지네이션 적용 #160

--- android/app/src/main/java/com/onair/hearit/data/repository/CategoryRepositoryImpl.kt
+++ android/app/src/main/java/com/onair/hearit/data/repository/CategoryRepositoryImpl.kt
@@ -3,14 +3,17 @@ package com.onair.hearit.data.repository
 import com.onair.hearit.data.datasource.CategoryRemoteDataSource
 import com.onair.hearit.data.toDomain
 import com.onair.hearit.domain.model.Category
+import com.onair.hearit.domain.model.PageResult
 import com.onair.hearit.domain.repository.CategoryRepository
 
 class CategoryRepositoryImpl(
     private val categoryDataSource: CategoryRemoteDataSource,
 ) : CategoryRepository {
-    override suspend fun getCategories(): Result<List<Category>> =
+    override suspend fun getCategories(
+        page: Int?,
+        size: Int?,
+    ): Result<PageResult<Category>> =
         handleResult {
-            val response = categoryDataSource.getCategories().getOrThrow()
-            response.map { it.toDomain() }
+            categoryDataSource.getCategories(page, size).getOrThrow().toDomain()
         }
 }
