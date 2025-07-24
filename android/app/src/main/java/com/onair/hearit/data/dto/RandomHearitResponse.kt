3f902c4cab80d238a170c3b790adb9eb0209d2b1
Ham BeomJoon <hbj0802@naver.com>
2025-07-24 15:04:17 +0900

    [AN] refactor: 랜덤 히어릿 Response 응답 필드 변경 #152

--- android/app/src/main/java/com/onair/hearit/data/dto/RandomHearitResponse.kt
+++ android/app/src/main/java/com/onair/hearit/data/dto/RandomHearitResponse.kt
@@ -5,16 +5,38 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class RandomHearitResponse(
-    @SerialName("createdAt")
-    val createdAt: String,
-    @SerialName("id")
-    val id: Long,
-    @SerialName("playTime")
-    val playTime: Int,
-    @SerialName("source")
-    val source: String,
-    @SerialName("summary")
-    val summary: String,
-    @SerialName("title")
-    val title: String,
-)
+    @SerialName("content")
+    val content: List<Content>,
+    @SerialName("page")
+    val page: Int,
+    @SerialName("size")
+    val size: Int,
+    @SerialName("totalPages")
+    val totalPages: Int,
+    @SerialName("totalElements")
+    val totalElements: Int,
+    @SerialName("isFirst")
+    val isFirst: Boolean,
+    @SerialName("isLast")
+    val isLast: Boolean,
+) {
+    @Serializable
+    data class Content(
+        @SerialName("id")
+        val id: Long,
+        @SerialName("title")
+        val title: String,
+        @SerialName("summary")
+        val summary: String,
+        @SerialName("source")
+        val source: String,
+        @SerialName("playTime")
+        val playTime: Int,
+        @SerialName("createdAt")
+        val createdAt: String,
+        @SerialName("isBookmarked")
+        val isBookmarked: Boolean,
+        @SerialName("bookmarkId")
+        val bookmarkId: Long,
+    )
+}
