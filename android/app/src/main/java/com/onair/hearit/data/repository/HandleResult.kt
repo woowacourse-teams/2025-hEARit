2cec90b95b96603ac02cda0cfe8ba0ad3242abe0
Ham BeomJoon <hbj0802@naver.com>
2025-07-28 17:29:16 +0900

    [AN] feat: handleResult 함수에 Crashlytics recordException 추가 #190

--- android/app/src/main/java/com/onair/hearit/data/repository/HandleResult.kt
+++ android/app/src/main/java/com/onair/hearit/data/repository/HandleResult.kt
@@ -1,8 +1,13 @@
 package com.onair.hearit.data.repository
 
 import com.onair.hearit.BuildConfig
+import com.onair.hearit.di.CrashlyticsProvider
 
 inline fun <T> handleResult(action: () -> T): Result<T> =
     runCatching(action).onFailure { throwable ->
-        if (BuildConfig.DEBUG) throwable.printStackTrace()
+        if (BuildConfig.DEBUG) {
+            throwable.printStackTrace()
+        } else {
+            CrashlyticsProvider.get().recordException(throwable)
+        }
     }
