3f04c20bcf2d675ed04539933c1183e5decc64f1
YuyoungRhee <bbclara@naver.com>
2025-07-29 22:59:28 +0900

    [BE] refactor: 롬복 적용 등 불필요한 코드 제거 #146

--- backend/src/main/java/com/onair/hearit/admin/domain/AdminUserDetails.java
+++ backend/src/main/java/com/onair/hearit/admin/domain/AdminUserDetails.java
@@ -2,20 +2,16 @@ package com.onair.hearit.admin.domain;
 
 import java.util.Collection;
 import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+@Getter
+@RequiredArgsConstructor
 public class AdminUserDetails implements UserDetails {
 
     private final Admin admin;
-
-    public AdminUserDetails(Admin admin) {
-        this.admin = admin;
-    }
-
-    public Admin getAdmin() {
-        return admin;
-    }
 
     @Override
     public String getUsername() {
@@ -29,26 +25,6 @@ public class AdminUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(); // 권한 없음
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
+        return List.of(); // 권한 필요 없음
     }
 }
--- backend/src/main/java/com/onair/hearit/auth/application/AdminUserDetailsService.java
+++ backend/src/main/java/com/onair/hearit/auth/application/AdminUserDetailsService.java
@@ -3,19 +3,17 @@ package com.onair.hearit.auth.application;
 import com.onair.hearit.admin.domain.Admin;
 import com.onair.hearit.admin.domain.AdminUserDetails;
 import com.onair.hearit.admin.infrastructure.AdminRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AdminUserDetailsService implements UserDetailsService {
 
     private final AdminRepository adminRepository;
-
-    public AdminUserDetailsService(AdminRepository repo) {
-        this.adminRepository = repo;
-    }
 
     @Override
     public UserDetails loadUserByUsername(String loginId) {
