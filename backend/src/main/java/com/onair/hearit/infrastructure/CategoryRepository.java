b340dc5734dc22b23731b31ef3ca1660a5d32862
byesol <bsj5967@naver.com>
2025-07-30 16:01:07 +0900

    [BE] feat: 카테고리 3개 조회 메서드 구현 #212

--- backend/src/main/java/com/onair/hearit/infrastructure/CategoryRepository.java
+++ backend/src/main/java/com/onair/hearit/infrastructure/CategoryRepository.java
@@ -1,7 +1,10 @@
 package com.onair.hearit.infrastructure;
 
 import com.onair.hearit.domain.Category;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findTop3ByOrderByIdAsc();
 }
