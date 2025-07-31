126791b9012f4abe15744d06befb1eb87c7f736a
Gabin Choi <choegabin171@gmail.com>
2025-08-01 02:41:18 +0900

    [BE] refactor: json 변환 시 중요 정보 포함되지 않도록 수정 #204

--- backend/src/main/java/com/onair/hearit/auth/dto/request/LoginRequest.java
+++ backend/src/main/java/com/onair/hearit/auth/dto/request/LoginRequest.java
@@ -1,7 +1,10 @@
 package com.onair.hearit.auth.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record LoginRequest(
         String localId,
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         String password
 ) {
 }
