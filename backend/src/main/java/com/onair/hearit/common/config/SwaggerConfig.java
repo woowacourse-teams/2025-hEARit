8926f38279a3b293bb2b4cbc14ffe9d51bd939c7
byesol <bsj5967@naver.com>
2025-07-31 09:30:06 +0900

    [BE] docs: rest docs 사용에 따른 스웨거 설정 제거  #205

--- backend/src/main/java/com/onair/hearit/common/config/SwaggerConfig.java
+++ backend/src/main/java/com/onair/hearit/common/config/SwaggerConfig.java
@@ -1,36 +1,36 @@
-package com.onair.hearit.common.config;
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-import java.util.List;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                        )
-                )
-                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
-                .info(new Info()
-                        .title("Hearit API")
-                        .description("히어릿 백엔드 API 문서")
-                        .version("v1"))
-                .servers(List.of(
-                        new Server().url("http://localhost:80").description("로컬 서버"),
-                        new Server().url("http://hearit.o-r.kr").description("운영 서버")
-                ));
-    }
-}
+//package com.onair.hearit.common.config;
+//
+//import io.swagger.v3.oas.models.Components;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.security.SecurityRequirement;
+//import io.swagger.v3.oas.models.security.SecurityScheme;
+//import io.swagger.v3.oas.models.servers.Server;
+//import java.util.List;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SwaggerConfig {
+//
+//    @Bean
+//    public OpenAPI openAPI() {
+//        return new OpenAPI()
+//                .components(new Components()
+//                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+//                                .type(SecurityScheme.Type.HTTP)
+//                                .scheme("bearer")
+//                                .bearerFormat("JWT")
+//                        )
+//                )
+//                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+//                .info(new Info()
+//                        .title("Hearit API")
+//                        .description("히어릿 백엔드 API 문서")
+//                        .version("v1"))
+//                .servers(List.of(
+//                        new Server().url("http://localhost:80").description("로컬 서버"),
+//                        new Server().url("http://hearit.o-r.kr").description("운영 서버")
+//                ));
+//    }
+//}
