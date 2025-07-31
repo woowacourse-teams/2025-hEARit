921876c8514517d1106a0dc4e34fe47d29423959
byesol <bsj5967@naver.com>
2025-08-01 03:05:20 +0900

    [BE] docs: MemberController 에 rest docs 내용 추가 #205

--- backend/src/test/java/com/onair/hearit/presentation/MemberControllerTest.java
+++ backend/src/test/java/com/onair/hearit/presentation/MemberControllerTest.java
@@ -1,11 +1,18 @@
 package com.onair.hearit.presentation;
 
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.Schema;
 import com.onair.hearit.auth.infrastructure.jwt.JwtTokenProvider;
 import com.onair.hearit.domain.Member;
 import com.onair.hearit.dto.response.MemberInfoResponse;
+import com.onair.hearit.fixture.IntegrationTest;
+import com.onair.hearit.utils.ApiDocumentUtils;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,8 +37,22 @@ class MemberControllerTest extends IntegrationTest {
         String token = generateToken(member);
 
         // when & then
-        MemberInfoResponse response = RestAssured.given().log().all()
+        MemberInfoResponse response = RestAssured.given(this.spec)
                 .header("Authorization", "Bearer " + token)
+                .filter(document("member-read-me",
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("Member API")
+                                .summary("내 정보 조회")
+                                .description("현재 로그인한 사용자의 정보를 조회합니다.")
+                                .requestHeaders(ApiDocumentUtils.getAuthorizationHeader())
+                                .responseSchema(Schema.schema("MemberInfoResponse"))
+                                .responseFields(
+                                        fieldWithPath("id").description("사용자 ID"),
+                                        fieldWithPath("nickname").description("닉네임"),
+                                        fieldWithPath("profileImage").description("프로필 이미지 URL")
+                                )
+                                .build()))
+                )
                 .when()
                 .get("/api/v1/members/me")
                 .then().log().all()
