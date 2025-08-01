5cedca86fc328fa7f3c7e1b02fd44c46b20565e3
byesol <bsj5967@naver.com>
2025-08-01 17:27:04 +0900

    [BE] refactor: 히어릿 검색 결과 필드에 키워드 추가 #227
    - 요약 제거

--- backend/src/main/java/com/onair/hearit/dto/response/HearitSearchResponse.java
+++ backend/src/main/java/com/onair/hearit/dto/response/HearitSearchResponse.java
@@ -1,19 +1,38 @@
 package com.onair.hearit.dto.response;
 
 import com.onair.hearit.domain.Hearit;
+import com.onair.hearit.domain.Keyword;
+import java.util.List;
 
 public record HearitSearchResponse(
         Long id,
         String title,
-        String summary,
-        Integer playTime
+        Integer playTime,
+        List<KeywordResponse> keywords
 ) {
-    public static HearitSearchResponse from(Hearit hearit) {
+    public static HearitSearchResponse from(Hearit hearit, List<Keyword> keywords) {
+        List<KeywordResponse> keywordResponses = getKeywordNames(keywords);
         return new HearitSearchResponse(
                 hearit.getId(),
                 hearit.getTitle(),
-                hearit.getSummary(),
-                hearit.getPlayTime()
+                hearit.getPlayTime(),
+                keywordResponses
         );
+    }
+
+    private static List<KeywordResponse> getKeywordNames(List<Keyword> keywords) {
+        return keywords.stream().map(KeywordResponse::from).toList();
+    }
+
+    private record KeywordResponse(
+            Long id,
+            String name
+    ) {
+        public static KeywordResponse from(Keyword keyword) {
+            return new KeywordResponse(
+                    keyword.getId(),
+                    keyword.getName()
+            );
+        }
     }
 }
