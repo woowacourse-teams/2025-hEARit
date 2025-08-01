6d7dd73c1c4fd5f4229b2b24497a2f19c1655111
gahyunkim <mapledt001@naver.com>
2025-08-01 13:34:13 +0900

    [AN] feat: PlaybackInfo를 얻기 위한 usecase 구현 #221

--- android/app/src/main/java/com/onair/hearit/domain/usecase/GetPlaybackInfoUseCase.kt
+++ android/app/src/main/java/com/onair/hearit/domain/usecase/GetPlaybackInfoUseCase.kt
@@ -3,17 +3,27 @@ package com.onair.hearit.domain.usecase
 import com.onair.hearit.domain.model.PlaybackInfo
 import com.onair.hearit.domain.repository.HearitRepository
 import com.onair.hearit.domain.repository.MediaFileRepository
+import com.onair.hearit.domain.repository.RecentHearitRepository
 import com.onair.hearit.domain.toPlaybackInfo
 
 class GetPlaybackInfoUseCase(
     private val hearitRepository: HearitRepository,
     private val mediaFileRepository: MediaFileRepository,
+    private val recentHearitRepository: RecentHearitRepository,
 ) {
     suspend operator fun invoke(hearitId: Long): Result<PlaybackInfo> =
         runCatching {
             val hearitInfo = hearitRepository.getHearit(hearitId).getOrThrow()
             val audioUrl = mediaFileRepository.getOriginalAudioUrl(hearitId).getOrThrow().url
+            val recentHearit = recentHearitRepository.getRecentHearit().getOrThrow()
 
-            hearitInfo.toPlaybackInfo(audioUrl, hearitInfo.title)
+            val lastPosition =
+                if (recentHearit?.id == hearitId) {
+                    recentHearit.lastPosition
+                } else {
+                    0L
+                }
+
+            hearitInfo.toPlaybackInfo(audioUrl, hearitInfo.title, lastPosition)
         }
 }
