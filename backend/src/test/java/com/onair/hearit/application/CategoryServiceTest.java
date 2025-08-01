e1a74f0870b3fdc827ec9f14212c78c1c5a6c7fa
byesol <bsj5967@naver.com>
2025-08-01 22:32:37 +0900

    [BE] refactor: TestFixture 활용하도록 변경 #227

--- backend/src/test/java/com/onair/hearit/application/CategoryServiceTest.java
+++ backend/src/test/java/com/onair/hearit/application/CategoryServiceTest.java
@@ -53,11 +53,11 @@ class CategoryServiceTest {
     @DisplayName("카테고리 목록 조회 시 페이지네이션이 적용되어 반환된다.")
     void getCategories_withPagination() {
         // given
-        Category category = dbHelper.insertCategory(new Category("category1", "#111"));
-        Category category1 = dbHelper.insertCategory(new Category("category2", "#222"));
-        Category category2 = dbHelper.insertCategory(new Category("category3", "#333"));
-        Category category3 = dbHelper.insertCategory(new Category("category4", "#444"));
-        Category category4 = dbHelper.insertCategory(new Category("category5", "#555"));
+        Category category1 = dbHelper.insertCategory(TestFixture.createFixedCategory());
+        Category category2 = dbHelper.insertCategory(TestFixture.createFixedCategory());
+        Category category3 = dbHelper.insertCategory(TestFixture.createFixedCategory());
+        Category category4 = dbHelper.insertCategory(TestFixture.createFixedCategory());
+        Category category5 = dbHelper.insertCategory(TestFixture.createFixedCategory());
 
         PagingRequest pagingRequest = new PagingRequest(1, 2);// page 1 (두 번째 페이지), size 2
 
@@ -67,10 +67,8 @@ class CategoryServiceTest {
         // then
         assertAll(() -> {
             assertThat(result.content()).hasSize(2);
-            assertThat(result.content()).extracting(CategoryResponse::name)
-                    .containsExactly("category3", "category4");
-            assertThat(result.content()).extracting(CategoryResponse::colorCode)
-                    .containsExactly("#333", "#444");
+            assertThat(result.content()).extracting(CategoryResponse::id)
+                    .containsExactly(category3.getId(), category4.getId());
         });
     }
 
@@ -78,8 +76,8 @@ class CategoryServiceTest {
     @DisplayName("히어릿 목록을 카테고리 조회 시 카테고리에 해당하는 히어릿만 반환한다.")
     void searchHearitsByCategory_onlyMatchingCategory() {
         // given
-        Category category1 = dbHelper.insertCategory(new Category("Spring", "#001"));
-        Category category2 = dbHelper.insertCategory(new Category("Java", "#002"));
+        Category category1 = dbHelper.insertCategory(TestFixture.createFixedCategory());
+        Category category2 = dbHelper.insertCategory(TestFixture.createFixedCategory());
         Hearit hearit1 = dbHelper.insertHearit(TestFixture.createFixedHearitWith(category1));
         Hearit hearit2 = dbHelper.insertHearit(TestFixture.createFixedHearitWith(category1));
         Hearit hearit3 = dbHelper.insertHearit(TestFixture.createFixedHearitWith(category2));
