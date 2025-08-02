842d8f9fc3024184aae31f6a22d9d7a0059ba8b2
byesol <bsj5967@naver.com>
2025-08-03 08:43:43 +0900

    [BE] refactor: 문서 내 파라미터 default 값 추가 #205

--- backend/src/test/java/com/onair/hearit/presentation/CategoryControllerTest.java
+++ backend/src/test/java/com/onair/hearit/presentation/CategoryControllerTest.java
@@ -49,8 +49,8 @@ class CategoryControllerTest extends IntegrationTest {
                                 .summary("전체 카테고리 목록 조회")
                                 .description("전체 카테고리 목록을 페이지별로 조회합니다.")
                                 .queryParameters(
-                                        parameterWithName("page").description("페이지 번호 (0부터 시작)"),
-                                        parameterWithName("size").description("페이지 당 항목 수 (기본 20)")
+                                        parameterWithName("page").description("페이지 번호 (0부터 시작)").defaultValue("0"),
+                                        parameterWithName("size").description("페이지 당 항목 수 (기본 20)").defaultValue("20")
                                 )
                                 .responseSchema(Schema.schema("PagedResponse"))
                                 .responseFields(
