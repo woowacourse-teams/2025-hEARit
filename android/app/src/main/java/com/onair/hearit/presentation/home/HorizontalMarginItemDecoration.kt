0d7999a7b47c867024e4a3f6a127bac9c6524701
Ham BeomJoon <hbj0802@naver.com>
2025-08-03 16:43:15 +0900

    [AN] refactor: recyclerView NO_POSITION 체크 추가 #233

--- android/app/src/main/java/com/onair/hearit/presentation/home/HorizontalMarginItemDecoration.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/home/HorizontalMarginItemDecoration.kt
@@ -14,6 +14,7 @@ class HorizontalMarginItemDecoration(
         state: RecyclerView.State,
     ) {
         val position = parent.getChildAdapterPosition(view)
+        if (position == RecyclerView.NO_POSITION) return
 
         when (position) {
             0 -> outRect.left = sideMargin
