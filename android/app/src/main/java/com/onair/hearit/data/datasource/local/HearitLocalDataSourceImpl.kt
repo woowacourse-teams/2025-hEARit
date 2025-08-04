03d791e015b015f784e17c5a30cc210c40cc49d5
Ham BeomJoon <hbj0802@naver.com>
2025-08-04 19:32:11 +0900

    [AN] refactor: local dataSource runCatching으로 변경 #240

--- android/app/src/main/java/com/onair/hearit/data/datasource/local/HearitLocalDataSourceImpl.kt
+++ android/app/src/main/java/com/onair/hearit/data/datasource/local/HearitLocalDataSourceImpl.kt
@@ -1,21 +1,19 @@
 package com.onair.hearit.data.datasource.local
 
-import com.onair.hearit.analytics.CrashlyticsLogger
 import com.onair.hearit.data.database.HearitDao
 import com.onair.hearit.data.database.RecentHearitEntity
 import com.onair.hearit.data.database.SearchHistoryEntity
-import com.onair.hearit.data.repository.handleResult
 
 class HearitLocalDataSourceImpl(
     private val hearitDao: HearitDao,
 ) : HearitLocalDataSource {
-    override suspend fun getRecentHearit(): Result<RecentHearitEntity?> = handleResult { hearitDao.getRecentHearit() }
+    override suspend fun getRecentHearit(): Result<RecentHearitEntity?> = runCatching { hearitDao.getRecentHearit() }
 
-    override suspend fun saveRecentHearit(entity: RecentHearitEntity): Result<Unit> = handleResult { hearitDao.insertRecentHearit(entity) }
+    override suspend fun saveRecentHearit(entity: RecentHearitEntity): Result<Unit> = runCatching { hearitDao.insertRecentHearit(entity) }
 
-    override suspend fun getKeywords(): Result<List<SearchHistoryEntity>> = handleResult { hearitDao.getKeywords() }
+    override suspend fun getKeywords(): Result<List<SearchHistoryEntity>> = runCatching { hearitDao.getKeywords() }
 
-    override suspend fun saveKeyword(keyword: SearchHistoryEntity): Result<Unit> = handleResult { hearitDao.insertKeyword(keyword) }
+    override suspend fun saveKeyword(keyword: SearchHistoryEntity): Result<Unit> = runCatching { hearitDao.insertKeyword(keyword) }
 
-    override suspend fun clearKeywords(): Result<Unit> = handleResult { hearitDao.deleteKeywords() }
+    override suspend fun clearKeywords(): Result<Unit> = runCatching { hearitDao.deleteKeywords() }
 }
