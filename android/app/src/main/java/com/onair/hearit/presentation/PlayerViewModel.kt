edb20c520b93b0c5331bb647cddc87c12d8cf9d8
gahyunkim <mapledt001@naver.com>
2025-08-04 14:58:54 +0900

    [AN] refactor: 재생 정보 불러오기 로직 삭제 #221

--- android/app/src/main/java/com/onair/hearit/presentation/PlayerViewModel.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/PlayerViewModel.kt
@@ -5,19 +5,13 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.onair.hearit.R
-import com.onair.hearit.domain.model.PlaybackInfo
 import com.onair.hearit.domain.model.RecentHearit
 import com.onair.hearit.domain.repository.RecentHearitRepository
-import com.onair.hearit.domain.usecase.GetPlaybackInfoUseCase
 import kotlinx.coroutines.launch
 
 class PlayerViewModel(
     private val recentHearitRepository: RecentHearitRepository,
-    private val getPlaybackInfoUseCase: GetPlaybackInfoUseCase,
 ) : ViewModel() {
-    private val _playbackInfo = SingleLiveData<PlaybackInfo>()
-    val playbackInfo: LiveData<PlaybackInfo> = _playbackInfo
-
     private val _recentHearit = MutableLiveData<RecentHearit?>()
     val recentHearit: LiveData<RecentHearit?> = _recentHearit
 
@@ -26,14 +20,6 @@ class PlayerViewModel(
 
     init {
         fetchRecentHearit()
-    }
-
-    fun preparePlayback(hearitId: Long) {
-        viewModelScope.launch {
-            getPlaybackInfoUseCase(hearitId)
-                .onSuccess { _playbackInfo.value = it }
-                .onFailure { _toastMessage.value = R.string.main_toast_load_player_hearit_fail }
-        }
     }
 
     private fun fetchRecentHearit() {
