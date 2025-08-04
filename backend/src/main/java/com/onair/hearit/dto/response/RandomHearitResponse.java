67312e66ee9a5ee6f153523a6c2ac5569dcb8469
San <goodsan0330@gmail.com>
2025-08-04 22:25:38 +0900

    [BE] refactor: 랜덤 히어릿 응답에 키워드 포함 #248
    * [BE] refactor: 불필요한 Hearit 정보 Response에서 제거 #248
    
    - RandomHearitResponse의 summary, source, playTime, createdAt 필드 제거
    
    * [BE] refactor: 카테고리 히어릿의 키워드 개수 상수 이름 변경 #248
    
    - KEYWORDS_PER_HEARIT_FROM_CATEGORY로 변경
    
    * [BE] refactor: 랜덤 히어릿 조회 시 키워드 리스트 필드 추가 #248
    
    * [BE] style: 랜덤 히어릿 ResponseDto에 개행 추가 #248
    
    * [BE] test: 랜덤 히어릿 ResponseDto에 키워드 포함 여부 확인 #248
    
    * [BE] refactor: 카테고리 별 히어릿에 대한 키워드 상수 이름 변경 #248
    
    * [BE] fix: 랜덤으로 인한 테스트 랜덤성 제거  #248
    
    * [BE] style: inner class에서 불필요한 클래스 이름 제거  #248
    
    * [BE] style: Service에 공백 제거  #248

--- backend/src/main/java/com/onair/hearit/dto/response/RandomHearitResponse.java
+++ backend/src/main/java/com/onair/hearit/dto/response/RandomHearitResponse.java
@@ -2,39 +2,53 @@ package com.onair.hearit.dto.response;
 
 import com.onair.hearit.domain.Bookmark;
 import com.onair.hearit.domain.Hearit;
-import java.time.LocalDateTime;
+import com.onair.hearit.domain.Keyword;
+import java.util.List;
 
 public record RandomHearitResponse(
         Long id,
         String title,
-        String summary,
-        String source,
-        Integer playTime,
-        LocalDateTime createdAt,
         Boolean isBookmarked,
-        Long bookmarkId
+        Long bookmarkId,
+        List<KeywordResponse> keywords
 ) {
-    public static RandomHearitResponse from(Hearit hearit) {
+    public static RandomHearitResponse from(Hearit hearit, List<Keyword> keywords) {
+        List<KeywordResponse> keywordResponses = getKeywordNames(keywords);
         return new RandomHearitResponse(
                 hearit.getId(),
                 hearit.getTitle(),
-                hearit.getSummary(),
-                hearit.getSource(),
-                hearit.getPlayTime(),
-                hearit.getCreatedAt(),
                 false,
-                null);
+                null,
+                keywordResponses
+        );
     }
 
-    public static RandomHearitResponse fromWithBookmark(Hearit hearit, Bookmark bookmark) {
+    public static RandomHearitResponse fromWithBookmark(Hearit hearit, Bookmark bookmark, List<Keyword> keywords) {
+        List<KeywordResponse> keywordResponses = getKeywordNames(keywords);
         return new RandomHearitResponse(
                 hearit.getId(),
                 hearit.getTitle(),
-                hearit.getSummary(),
-                hearit.getSource(),
-                hearit.getPlayTime(),
-                hearit.getCreatedAt(),
                 true,
-                bookmark.getId());
+                bookmark.getId(),
+                keywordResponses
+        );
+    }
+
+    private static List<KeywordResponse> getKeywordNames(List<Keyword> keywords) {
+        return keywords.stream()
+                .map(KeywordResponse::from)
+                .toList();
+    }
+
+    private record KeywordResponse(
+            Long id,
+            String name
+    ) {
+        public static KeywordResponse from(Keyword keyword) {
+            return new KeywordResponse(
+                    keyword.getId(),
+                    keyword.getName()
+            );
+        }
     }
 }
