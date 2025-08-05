549259ddaa7d956b91ec150a25dde14125c86084
의엉 <127360730+YuyoungRhee@users.noreply.github.com>
2025-08-05 18:31:39 +0900

    [BE] feat: 엑세스토큰 유효성 검증 API 추가 #259
    * [BE] feat: 엑세스토큰 유효성 검증 기능 추가 #259
    
    * [BE] feat: 엑세스토큰 검증 실패 시 로그 추가 #259
    
    * [BE] feat: 엑세스토큰 검증을 헤더를 통해서 하도록 변경 #259
    
    * [BE] test: 테스트 오타 수정 #259

--- backend/src/main/java/com/onair/hearit/auth/presentation/AuthController.java
+++ backend/src/main/java/com/onair/hearit/auth/presentation/AuthController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,6 +47,11 @@ public class AuthController {
     public ResponseEntity<TokenReissueResponse> reissue(@RequestBody TokenReissueRequest request) {
         String newAccessToken = authService.reissue(request.refreshToken());
         return ResponseEntity.ok(new TokenReissueResponse(newAccessToken));
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<Void> checkAccessToken() {
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/logout")
