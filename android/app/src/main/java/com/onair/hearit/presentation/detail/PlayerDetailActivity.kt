32c29146568568f7d3817219881ae663e4c9ca03
Ham BeomJoon <hbj0802@naver.com>
2025-08-06 15:43:27 +0900

    [AN] style: ktlint 적용 #276

--- android/app/src/main/java/com/onair/hearit/presentation/detail/PlayerDetailActivity.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/detail/PlayerDetailActivity.kt
@@ -39,8 +39,6 @@ import com.onair.hearit.databinding.ActivityPlayerDetailBinding
 import com.onair.hearit.di.AnalyticsProvider
 import com.onair.hearit.di.CrashlyticsProvider
 import com.onair.hearit.domain.model.Hearit
-import com.onair.hearit.presentation.PlayerViewModel
-import com.onair.hearit.presentation.PlayerViewModelFactory
 import com.onair.hearit.presentation.detail.script.ScriptFragment
 import com.onair.hearit.service.PlaybackService
 import kotlinx.coroutines.launch
