3dabcfe60b69a605f4f14c28222f67a216c60534
Gabin Choi <choegabin171@gmail.com>
2025-08-06 15:04:30 +0900

    [BE] rename: S3Exception -> AdminFileException으로 변경 #246

---
+++ backend/src/main/java/com/onair/hearit/admin/exception/AdminFileException.java
@@ -0,0 +1,8 @@
+package com.onair.hearit.admin.exception;
+
+public class AdminFileException extends RuntimeException {
+
+    public AdminFileException(String message, Throwable e) {
+        super(message, e);
+    }
+}
