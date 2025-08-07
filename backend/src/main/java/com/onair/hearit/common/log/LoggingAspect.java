76579c185971ac738069289f784ea166f86b3f67
Gabin Choi <choegabin171@gmail.com>
2025-08-07 20:46:53 +0900

    [BE] style: 코드 정렬 #297

--- backend/src/main/java/com/onair/hearit/common/log/LoggingAspect.java
+++ backend/src/main/java/com/onair/hearit/common/log/LoggingAspect.java
@@ -14,7 +14,6 @@ import java.util.Objects;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
