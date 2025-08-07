2607677a070975c0dfa5e99da8db5be0f124762d
Ham BeomJoon <hbj0802@naver.com>
2025-08-07 20:14:36 +0900

    [AN] style: ktlint 적용 #290

--- android/app/src/main/java/com/onair/hearit/presentation/detail/PlayerDetailActivity.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/detail/PlayerDetailActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
