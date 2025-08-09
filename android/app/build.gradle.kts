e826a2d7f261f38fa585dda61d4fb8b273903941
Ham BeomJoon <hbj0802@naver.com>
2025-08-09 15:38:43 +0900

    [AN] chore: 기타 수정 #312

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
     applicationVariants.all {
         outputs.all {
             val outputImpl = this as com.android.build.gradle.internal.api.ApkVariantOutputImpl
-            val newFileName = "hEARit-$versionName-$name.apk"
+            val newFileName = "hEARit-$name.apk"
             outputImpl.outputFileName = newFileName
         }
     }
