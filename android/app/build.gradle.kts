018b184733834d8c27762ebfe68b3d4c161925cb
Ham BeomJoon <hbj0802@naver.com>
2025-08-09 13:26:29 +0900

    [AN] build: firebase CD #312

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,6 +48,14 @@ android {
             null
         }
 
+    applicationVariants.all {
+        outputs.all {
+            val outputImpl = this as com.android.build.gradle.internal.api.ApkVariantOutputImpl
+            val newFileName = "hEARit-$versionName-$name.apk"
+            outputImpl.outputFileName = newFileName
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
