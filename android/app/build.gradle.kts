cec63fe2651121d2db9f5e7706bc034069577395
Ham BeomJoon <hbj0802@naver.com>
2025-08-10 18:56:14 +0900

    [AN] chore: 버그 수정 #334

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -53,16 +53,6 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-
-            manifestPlaceholders["enableCrashReporting"] = "true"
-        }
-
-        debug {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
