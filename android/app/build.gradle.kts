4ffe0df6fa97c53086f6cd14aca1c150e30dd5f9
Ham BeomJoon <hbj0802@naver.com>
2025-08-12 20:58:25 +0900

    [AN] build: v1.0.09 버전 배포 #340

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.onair.hearit"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1008
-        versionName = "1.0.08"
+        versionCode = 1009
+        versionName = "1.0.09"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
