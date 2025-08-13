7b7b5f9dd8992cfd4ed2d969dbe0b6de0441226a
byesol <bsj5967@naver.com>
2025-08-13 14:26:58 +0900

    [BE] refactor: FileUrls로 validate 책임 변경  #354

--- backend/src/main/java/com/onair/hearit/domain/FileUrls.java
+++ backend/src/main/java/com/onair/hearit/domain/FileUrls.java
@@ -1,6 +1,8 @@
 package com.onair.hearit.domain;
 
+import com.onair.hearit.common.exception.custom.InvalidInputException;
 import jakarta.persistence.Embeddable;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,21 +17,47 @@ public class FileUrls {
     private String scriptUrl;
 
     public FileUrls(final String originalAudioUrl, final String shortAudioUrl, final String scriptUrl) {
+        validateAll(originalAudioUrl, shortAudioUrl, scriptUrl);
         this.originalAudioUrl = originalAudioUrl;
         this.shortAudioUrl = shortAudioUrl;
         this.scriptUrl = scriptUrl;
     }
 
-    // Wither 메서드 추가
-    public FileUrls withOriginalAudioUrl(String newOriginalAudioUrl) {
+    private void validateAll(String originalAudioPath, String shortAudioPath, String scriptFilePath) {
+        FileType.ORIGINAL.validateFilename(originalAudioPath);
+        FileType.SHORT.validateFilename(shortAudioPath);
+        FileType.SCRIPT.validateFilename(scriptFilePath);
+        validateSameUuid(List.of(originalAudioPath, shortAudioPath, scriptFilePath));
+    }
+
+    private void validateSameUuid(List<String> fileNames) {
+        String uuid = extractUuid(fileNames.getFirst());
+        for (String fileName : fileNames) {
+            if (!uuid.equals(extractUuid(fileName))) {
+                throw new InvalidInputException("파일들이 동일한 리소스를 나타내지 않습니다.");
+            }
+        }
+    }
+
+    private String extractUuid(String filename) {
+        int underscoreIndex = filename.indexOf('_');
+        int dotIndex = filename.lastIndexOf('.');
+        if (underscoreIndex == -1 || dotIndex == -1 || underscoreIndex >= dotIndex) {
+            throw new InvalidInputException("파일명 형식이 올바르지 않습니다: " + filename);
+        }
+        return filename.substring(underscoreIndex + 1, dotIndex);
+    }
+
+
+    public FileUrls updateOriginalAudioUrl(String newOriginalAudioUrl) {
         return new FileUrls(newOriginalAudioUrl, this.shortAudioUrl, this.scriptUrl);
     }
 
-    public FileUrls withShortAudioUrl(String newShortAudioUrl) {
+    public FileUrls updateShortAudioUrl(String newShortAudioUrl) {
         return new FileUrls(this.originalAudioUrl, newShortAudioUrl, this.scriptUrl);
     }
 
-    public FileUrls withScriptUrl(String newScriptUrl) {
+    public FileUrls updateScriptUrl(String newScriptUrl) {
         return new FileUrls(this.originalAudioUrl, this.shortAudioUrl, newScriptUrl);
     }
 }
