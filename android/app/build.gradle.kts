e132307195280b58bf948e20929975d76cf78032
Ham BeomJoon <hbj0802@naver.com>
2025-08-14 23:26:48 +0900

    [AN] build: v1.0.12 업그레이드 #387

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.onair.hearit"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1010
-        versionName = "1.0.10"
+        versionCode = 1012
+        versionName = "1.0.12"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
