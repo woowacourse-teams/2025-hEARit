789ee68b73b59a1331dc1d1d4e38d175a86f9509
Ham BeomJoon <37996727+HamBeomJoon@users.noreply.github.com>
2025-08-14 18:25:05 +0900

    [AN] build: v1.0.11 업그레이드

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.onair.hearit"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1010
-        versionName = "1.0.10"
+        versionCode = 1011
+        versionName = "1.0.11"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
