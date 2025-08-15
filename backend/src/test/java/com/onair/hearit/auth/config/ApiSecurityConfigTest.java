35b2db44bd7bf58cb37d6f0bb1846757dcb8bdb2
Gabin Choi <choegabin171@gmail.com>
2025-08-15 22:16:38 +0900

    [BE] test: test 제거 #338

--- backend/src/test/java/com/onair/hearit/auth/config/ApiSecurityConfigTest.java
+++ backend/src/test/java/com/onair/hearit/auth/config/ApiSecurityConfigTest.java
@@ -30,18 +30,6 @@ class ApiSecurityConfigTest extends IntegrationTest {
     }
 
     @Test
-    @DisplayName("OPEN GET API 경로는 HTTP Method get 제외 인증 없이 접근할 수 없다")
-    void canAccessWhitelistedPathWithoutAuth() {
-        // when & then
-        RestAssured.given().log().all()
-                .when()
-                .post("/api/v1/categories")
-                .then().log().all()
-                .statusCode(HttpStatus.FORBIDDEN);
-    }
-
-
-    @Test
     @DisplayName("보호된 API는 인증 없이 접근할 수 없다")
     void cannotAccessProtectedApiWithoutToken() {
         RestAssured.given().log().all()
