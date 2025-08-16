e471859dd0f5a948311f1231c2421fe22c3de003
rosemin928 <rosemin928@naver.com>
2025-08-16 23:34:35 +0900

    [AN] refactor: PlaylistBottomSheet 레이아웃 높이 설정 및 peekHeight 동적 조정 #384

--- android/app/src/main/java/com/onair/hearit/presentation/main/playlist/PlaylistBottomSheet.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/main/playlist/PlaylistBottomSheet.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.onair.hearit.databinding.BottomSheetPlaylistBinding
 
@@ -42,12 +44,29 @@ class PlaylistBottomSheet : BottomSheetDialogFragment() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        val bottomSheetDialog = dialog as? BottomSheetDialog ?: return
+        val behavior = bottomSheetDialog.behavior
+
+        (requireView().parent as View).layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+
+        behavior.peekHeight = (resources.displayMetrics.heightPixels * INITIAL_PEEK_RATIO).toInt()
+        behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        behavior.isFitToContents = false
+        behavior.skipCollapsed = false
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
     companion object {
+        private const val INITIAL_PEEK_RATIO = 0.5
+
         fun newInstance(): PlaylistBottomSheet = PlaylistBottomSheet()
     }
 }
