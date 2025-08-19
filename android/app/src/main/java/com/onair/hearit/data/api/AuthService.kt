76be4510b6970f54c3578c3566bd062340d48a13
Ham BeomJoon <hbj0802@naver.com>
2025-08-19 11:02:34 +0900

    [AN] style: ktlint 적용 #417

--- android/app/src/main/java/com/onair/hearit/data/api/AuthService.kt
+++ android/app/src/main/java/com/onair/hearit/data/api/AuthService.kt
@@ -8,7 +8,6 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface AuthService {
