21881cdb6c7b403c2447097b6abba5e05679226a
gahyunkim <mapledt001@naver.com>
2025-08-20 15:34:46 +0900

    [AN] refactor: Activity.RESULT_OK -> RESULT_OK로 수정 #414

--- android/app/src/main/java/com/onair/hearit/presentation/MainActivity.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.onair.hearit.presentation
 
-import android.app.Activity
 import android.content.ComponentName
 import android.content.Intent
 import android.graphics.Color
@@ -89,7 +88,7 @@ class MainActivity :
     private fun setupResultLauncher() {
         detailResultLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
+                if (result.resultCode == RESULT_OK) {
                     val detailResult =
                         result.data.toDetailResult() ?: return@registerForActivityResult
                     detailResult.navigate(this)
