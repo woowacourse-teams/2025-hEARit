cbd02735b3a42889aa729295a9989f5975d584fa
Ham BeomJoon <hbj0802@naver.com>
2025-08-21 09:06:46 +0900

    [AN] fix: 탐색화면 binding NPE 처리 #469

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.onair.hearit"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10101
-        versionName = "1.1.1"
+        versionCode = 10103
+        versionName = "1.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         manifestPlaceholders += mapOf()
