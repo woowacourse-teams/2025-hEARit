30c4a4855c5cfe218f596590a1d6c63197977d66
Gabin Choi <choegabin171@gmail.com>
2025-09-05 20:07:00 +0900

    [BE] chore: member, explore_score table migration #486
    - member table uuid 추가
    - explore_score table user_uuid 추가, member_id 삭제, 데이터 삭제

--- backend/src/main/resources/db/migration/V202509051943__member_explore_uuid.sql
+++ backend/src/main/resources/db/migration/V202509051943__member_explore_uuid.sql
@@ -5,6 +5,10 @@ ALTER TABLE member
 -- explore_score 테이블의 기존 데이터 삭제
 TRUNCATE TABLE explore_score;
 
+-- explore_score 테이블에 member_id 컬럼 삭제
+ALTER TABLE explore_score
+    DROP COLUMN member_id;
+
 -- explore_score 테이블에 uuid 컬럼 문자열 32자 추가
 ALTER TABLE explore_score
-    ADD COLUMN uuid CHAR(32);
+    ADD COLUMN user_uuid CHAR(32);
