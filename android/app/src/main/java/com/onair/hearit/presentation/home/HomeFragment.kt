697b8125fc69006f45c10dcee569cbef48ebbe03
Ham BeomJoon <hbj0802@naver.com>
2025-09-10 10:50:55 +0900

    [AN] refactor: 홈 화면에서 카테고리 목록 진입 후 뒤로가기 시 다시 홈 화면으로 이동 #485

--- android/app/src/main/java/com/onair/hearit/presentation/home/HomeFragment.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/home/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
@@ -22,17 +21,14 @@ import com.onair.hearit.di.AnalyticsProvider
 import com.onair.hearit.domain.model.Direction
 import com.onair.hearit.domain.model.RecommendHearit
 import com.onair.hearit.domain.model.RecommendHearits
+import com.onair.hearit.domain.model.SearchInput
 import com.onair.hearit.presentation.DrawerClickListener
 import com.onair.hearit.presentation.HearitClickListener
-import com.onair.hearit.presentation.IntentKeys.CATEGORY_COLOR_KEY
-import com.onair.hearit.presentation.IntentKeys.CATEGORY_ID_KEY
-import com.onair.hearit.presentation.IntentKeys.CATEGORY_KEY
-import com.onair.hearit.presentation.IntentKeys.CATEGORY_NAME_KEY
 import com.onair.hearit.presentation.MainActivity
 import com.onair.hearit.presentation.MainViewModel
 import com.onair.hearit.presentation.detail.PlayerDetailActivity
 import com.onair.hearit.presentation.explore.ExploreFragment
-import com.onair.hearit.presentation.search.SearchFragment
+import com.onair.hearit.presentation.search.category.SearchCategoryFragment
 
 class HomeFragment :
     Fragment(),
@@ -238,21 +234,13 @@ class HomeFragment :
     ) {
         parentFragmentManager
             .beginTransaction()
-            .replace(R.id.fragment_container_view, SearchFragment())
-            .addToBackStack(null)
+            .replace(
+                R.id.fragment_container_view,
+                SearchCategoryFragment.newInstance(
+                    SearchInput.Category(id, name, colorCode),
+                ),
+            ).addToBackStack(null)
             .commit()
-
-        (requireActivity() as MainActivity).selectTab(R.id.nav_search)
-        parentFragmentManager.executePendingTransactions()
-
-        parentFragmentManager.setFragmentResult(
-            CATEGORY_KEY,
-            bundleOf(
-                CATEGORY_ID_KEY to id,
-                CATEGORY_NAME_KEY to name,
-                CATEGORY_COLOR_KEY to colorCode,
-            ),
-        )
     }
 
     private fun navigateToPlayerDetail(hearitId: Long) {
