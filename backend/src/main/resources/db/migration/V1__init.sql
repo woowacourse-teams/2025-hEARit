5a46e261ef60ccfa6af97aea112ab48fe8aaee85
백승주 <bsj5967@naver.com>
2025-09-19 10:57:50 +0900

    [BE] fix: flyway의 v1 파일 수정 #548
    * [BE] fix: flyway의 v1 파일 수정 #548
    
    * [BE] fix: flyway의 v1 오타 수정 #548

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -64,8 +64,7 @@ CREATE TABLE `explore_score` (
                                  `hearit_id` bigint NOT NULL,
                                  `score` double NOT NULL,
                                  `cursor_id` bigint DEFAULT NULL,
-                                 PRIMARY KEY (`id`),
-                                 CONSTRAINT `uq_explore_score_member_hearit` UNIQUE (`member_id`, `hearit_id`)
+                                 PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `hearit_keyword` (
