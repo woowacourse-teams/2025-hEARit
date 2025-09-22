5e3740ae21b67dbcf11f2258c40d9f5c4a9184e2
gahyunkim <mapledt001@naver.com>
2025-09-22 17:26:28 +0900

    [AN] feat: MainActivity에 컨트롤러 연결 #418

--- android/app/src/main/java/com/onair/hearit/presentation/main/MainActivity.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/main/MainActivity.kt
@@ -98,6 +98,7 @@ class MainActivity :
         setupNavigation()
         setupDrawer()
         observeViewModel()
+        attachController()
         showFragment(HomeFragment())
         setupBottomControllerClick()
     }
