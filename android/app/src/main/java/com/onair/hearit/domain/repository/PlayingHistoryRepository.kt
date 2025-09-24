4296aee662821bb12b71cb1fe71ece2d3c2611ed
Ham BeomJoon <hbj0802@naver.com>
2025-09-24 10:38:10 +0900

    [AN] feat: 최근 들은 히어릿 dataSource, repository 추가 #585

--- android/app/src/main/java/com/onair/hearit/domain/repository/PlayingHistoryRepository.kt
+++ android/app/src/main/java/com/onair/hearit/domain/repository/PlayingHistoryRepository.kt
@@ -1,6 +1,10 @@
 package com.onair.hearit.domain.repository
 
+import com.onair.hearit.domain.model.PlayingHistoryHearit
+
 interface PlayingHistoryRepository {
+    suspend fun getPlayingHistories(): Result<List<PlayingHistoryHearit>>
+
     suspend fun addPlayingHistory(
         hearitId: Long,
         lastPlayTime: Long,
