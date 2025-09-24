17a35bc584ab8db33f085f9b1394c3f40fc9c718
Ham BeomJoon <hbj0802@naver.com>
2025-09-24 10:37:58 +0900

    [AN] feat: 최근 들은 히어릿 service, DTO 추가 #585

--- android/app/src/main/java/com/onair/hearit/data/api/PlayingHistoryService.kt
+++ android/app/src/main/java/com/onair/hearit/data/api/PlayingHistoryService.kt
@@ -1,11 +1,16 @@
 package com.onair.hearit.data.api
 
 import com.onair.hearit.data.dto.PlayingHistoryRequest
+import com.onair.hearit.data.dto.PlayingHistoryResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface PlayingHistoryService {
+    @GET("/api/v1/playing-histories/hearits")
+    suspend fun getPlayingHistories(): Response<List<PlayingHistoryResponse>>
+
     @POST("api/v1/playing-histories")
     suspend fun postPlayingHistory(
         @Body playingHistoryRequest: PlayingHistoryRequest,
