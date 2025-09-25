f96b21baadc48796b09fe8a93de8ec754dd1c3a3
Ham BeomJoon <hbj0802@naver.com>
2025-09-25 15:35:11 +0900

    [AN] build: v1.2.4 버전 릴리즈 #609

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.onair.hearit"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10203
-        versionName = "1.2.3"
+        versionCode = 10204
+        versionName = "1.2.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         manifestPlaceholders += mapOf()
