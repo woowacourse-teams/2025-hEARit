125053ba8884014956f96e881940a32100dfc26a
YuyoungRhee <bbclara@naver.com>
2025-09-26 16:41:32 +0900

    [BE] fix: SecurityFilterChain admin과 app 순서 문제 해결 #611

--- backend/admin/src/main/java/com/onair/hearit/auth/config/AdminSecurityConfig.java
+++ backend/admin/src/main/java/com/onair/hearit/auth/config/AdminSecurityConfig.java
@@ -4,6 +4,7 @@ import com.onair.hearit.auth.application.AdminUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,6 +15,7 @@ public class AdminSecurityConfig {
     private final AdminUserDetailsService adminUserDetailsService;
 
     @Bean
+    @Order(1)
     public SecurityFilterChain adminFilterChain(HttpSecurity http) throws Exception {
         return http
                 .securityMatcher("/admin/**", "/api/v1/admin/**")
