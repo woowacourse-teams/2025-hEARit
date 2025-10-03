1f51ad5e96bf56286736250183df2bd96ef5ece8
Gabin Choi <choegabin171@gmail.com>
2025-10-04 02:57:45 +0900

    [BE] refactor: WebConfig 클래스 이름을 ConverterWebConfig으로 변경 #643

--- backend/app/src/main/java/com/onair/hearit/app/bookmark/ConverterWebConfig.java
+++ backend/app/src/main/java/com/onair/hearit/app/bookmark/ConverterWebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class ConverterWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
