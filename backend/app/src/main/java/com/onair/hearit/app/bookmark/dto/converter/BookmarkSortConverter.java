30e15e6426ada5a99ee9f2d9316b499bb0666cdb
Gabin Choi <choegabin171@gmail.com>
2025-10-05 21:24:16 +0900

    [BE] refactor: BookmarkSortConverter에서 정렬 처리 로직 개선 #643
    - 입력 값에서 빈 문자열 필터링 로직 추가
    - single-value, two-value 정렬 조건 처리 방식 개선
    - 유효하지 않은 입력 값 예외 처리 로직 추가

--- backend/app/src/main/java/com/onair/hearit/app/bookmark/dto/converter/BookmarkSortConverter.java
+++ backend/app/src/main/java/com/onair/hearit/app/bookmark/dto/converter/BookmarkSortConverter.java
@@ -1,16 +1,25 @@
 package com.onair.hearit.app.bookmark.dto.converter;
 
 import com.onair.hearit.app.bookmark.dto.param.BookmarkSort;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.core.convert.converter.Converter;
 
 public class BookmarkSortConverter implements Converter<String, BookmarkSort> {
 
     @Override
     public BookmarkSort convert(String source) {
-        String[] sources = source.split(",");
-        if (sources.length == 2) {
-            return BookmarkSort.of(sources[0].trim(), sources[1].trim());
+        List<String> sources = Arrays.stream(source.split(","))
+                .map(String::trim)
+                .filter(s -> !s.isEmpty())
+                .toList();
+
+        if (sources.size() == 1) {
+            return BookmarkSort.from(sources.get(0));
         }
-        return BookmarkSort.from(sources[0].trim());
+        if (sources.size() == 2) {
+            return BookmarkSort.of(sources.get(0), sources.get(1));
+        }
+        throw new IllegalArgumentException();
     }
 }
