e1dd9ba11d088e36f44bc96620dc876504129571
YuyoungRhee <bbclara@naver.com>
2025-10-05 23:46:21 +0900

    [BE] refactor: 파라미터용 dto에 ModelAttribute 어노테이션 명시 #626

--- backend/app/src/main/java/com/onair/hearit/app/recommendation/presentation/RecommendationController.java
+++ backend/app/src/main/java/com/onair/hearit/app/recommendation/presentation/RecommendationController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,7 +22,7 @@ public class RecommendationController {
             "/api/v1/hearits/recommend-category" /* To be deprecated */})
     public ResponseEntity<List<RecommendationByCategoryResponse>> readRecommendationsByCategory(
             @AuthenticationPrincipal RequestUser requestUser,
-            RecommendationRequest request) {
+            @ModelAttribute RecommendationRequest request) {
         List<RecommendationByCategoryResponse> responses =
                 recommendationService.getCategoryRecommendations(
                         requestUser.getUserInfo(),
