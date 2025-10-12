aade2048ac23237bf26f8d7f8e7bf47a7de3b13d
Gabin Choi <choegabin171@gmail.com>
2025-10-12 19:56:18 +0900

    [BE] refactor: ResponseLog 클래스를 ResponseLogProperty로 리팩토링 #650
    - ResponseLog 클래스를 ResponseLogProperty로 대체하여 구조 개선
    - 불필요한 ResponseLog 관련 파일 삭제

---
+++ backend/core/src/main/java/com/onair/hearit/core/log/dto/ResponseLogProperty.java
@@ -0,0 +1,45 @@
+package com.onair.hearit.core.log.dto;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.slf4j.MDC;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ResponseLogProperty implements LogProperty {
+
+    private final String endPoint;
+    private final long timeTakenMs;
+    private final int status;
+    private final int bodySizeBytes;
+
+    public static ResponseLogProperty of(String endPoint, ResponseEntity<?> responseEntity) {
+        long timeTakenMs = calculateTimeTakenMs();
+        int status = responseEntity.getStatusCode().value();
+        int responseSizeBytes = getResponseBodySize(responseEntity);
+        return new ResponseLogProperty(endPoint, timeTakenMs, status, responseSizeBytes);
+    }
+
+    private static int getResponseBodySize(ResponseEntity<?> responseEntity) {
+        if (responseEntity.hasBody() && responseEntity.getBody() != null) {
+            return responseEntity.getBody().toString().getBytes(StandardCharsets.UTF_8).length;
+        }
+        return 0;
+    }
+
+    private static long calculateTimeTakenMs() {
+        return Optional.ofNullable(MDC.get("startTime"))
+                .map(Long::parseLong)
+                .map(startTime -> System.currentTimeMillis() - startTime)
+                .orElse(-1L);
+    }
+
+    @Override
+    public LogEvent getlogEvent() {
+        return LogEvent.RESPONSE;
+    }
+}
