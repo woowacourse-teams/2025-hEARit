48d2b80c27a20cdda4ae76ce22b5561b132dcf34
Gabin Choi <choegabin171@gmail.com>
2025-10-12 16:32:56 +0900

    [BE] feat: MDC IP, 디바이스 정보 추가 #650
    - 요청 시 IP, 디바이스 모델, 앱 버전 정보 추가 기록하도록 필터 수정
    - `Order` 어노테이션 추가로 필터 체인 우선순위 설정
    - 필터 처리 종료 후 MDC 값 초기화 로직 추가

--- backend/core/src/main/java/com/onair/hearit/core/log/MdcSetupFilter.java
+++ backend/core/src/main/java/com/onair/hearit/core/log/MdcSetupFilter.java
@@ -7,27 +7,39 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+/**
+ * 필터 체인에서 가장 먼저 실행되고 가장 마지막에 finally가 호출되는 Servlet Filter.
+ * <p>
+ * 이는 요청 처리 중 유지되어야 하는 MDC 값이 의도치 않게 삭제되지 않도록 하기 위함이다.
+ * </p>
+ */
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class MdcSetupFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-
-        MDC.put("server.port", String.valueOf(httpServletRequest.getLocalPort()));
-        MDC.put("id", UUID.randomUUID().toString());
+        
         MDC.put("ip", httpServletRequest.getRemoteAddr());
-        MDC.put("httpMethod", httpServletRequest.getMethod());
-        MDC.put("requestUri", httpServletRequest.getRequestURI());
+        MDC.put("timestamp", LocalDateTime.now(ZoneId.of("Asia/Seoul")).toString());
+        MDC.put("deviceModel", httpServletRequest.getHeader("Device-Model"));
+        MDC.put("appVersion", httpServletRequest.getHeader("App-Version"));
+
+        /* latencyTime을 위한 value */
         MDC.put("startTime", String.valueOf(System.currentTimeMillis()));
-
-        chain.doFilter(request, response);
-
-        MDC.clear();
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
     }
 }
