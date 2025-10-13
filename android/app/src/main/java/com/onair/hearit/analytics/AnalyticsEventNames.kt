f7d15e1c139f0b98501ddb30cdee80fcd821458d
Ham BeomJoon <hbj0802@naver.com>
2025-10-13 17:21:35 +0900

    [AN] feat: 검색화면에서 키워드 검색, 카테고리 선택 Analytics에 추가 #641

--- android/app/src/main/java/com/onair/hearit/analytics/AnalyticsEventNames.kt
+++ android/app/src/main/java/com/onair/hearit/analytics/AnalyticsEventNames.kt
@@ -3,10 +3,12 @@ package com.onair.hearit.analytics
 object AnalyticsEventNames {
     const val HOME_EXPLORE_SELECTED = "home_explore_selected"
     const val SEARCH_CATEGORY_SELECTED = "search_category_selected"
+    const val SEARCH_KEYWORD_ENTERED = "search_keyword_entered"
     const val DETAIL_KEYWORD_SELECTED = "detail_keyword_selected"
     const val DETAIL_CATEGORY_SELECTED = "detail_category_selected"
     const val DETAIL_SOURCE_SELECTED = "detail_source_selected"
     const val LOGIN_EVENT = "login_event"
     const val EXPLORE_TO_DETAIL = "explore_to_detail"
     const val EXPLORE_SWIPE = "explore_swipe"
+    const val LIBRARY_TO_DETAIL = "library_to_detail"
 }
