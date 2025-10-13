6123362d8fd8f172a502c3957bf8e37f1f7a40ef
JO-eusan <goodsan0330@gmail.com>
2025-10-13 21:06:26 +0900

    refactor: Profile Active 설정 제거 #653

--- backend/core/src/main/java/com/onair/hearit/core/config/DataSourceConfig.java
+++ backend/core/src/main/java/com/onair/hearit/core/config/DataSourceConfig.java
@@ -13,11 +13,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 @Configuration
-//@Profile("!integration-test")
 public class DataSourceConfig {
 
     public static final String MASTER_PROPERTY = "spring.datasource.master";
