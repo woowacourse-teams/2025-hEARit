11485ae989454b647cdb889492362b4e2bc99386
JO-eusan <goodsan0330@gmail.com>
2025-10-13 15:53:21 +0900

    feat: Master/Slave 연결 DataSource 설정 #653

---
+++ backend/core/src/main/java/com/onair/hearit/core/config/DataSourceConfig.java
@@ -0,0 +1,64 @@
+package com.onair.hearit.core.config;
+
+import com.onair.hearit.core.infrastructure.datasource.DataSourceType;
+import com.onair.hearit.core.infrastructure.datasource.RoutingDataSource;
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.HashMap;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+@Configuration
+@Profile("!integration-test")
+public class DataSourceConfig {
+
+    public static final String MASTER_PROPERTY = "spring.datasource.master";
+    public static final String SLAVE_PROPERTY = "spring.datasource.slave";
+
+    @Bean
+    @ConfigurationProperties(MASTER_PROPERTY)
+    public DataSource masterDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    @ConfigurationProperties(SLAVE_PROPERTY)
+    public DataSource slaveDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    @DependsOn({"masterDataSource", "slaveDataSource"})
+    public DataSource routingDataSource(
+            @Qualifier("masterDataSource") DataSource master,
+            @Qualifier("slaveDataSource") DataSource slave) {
+        Map<Object, Object> dataSources = new HashMap<>();
+        dataSources.put(DataSourceType.MASTER, master);
+        dataSources.put(DataSourceType.SLAVE, slave);
+
+        RoutingDataSource routingDataSource = new RoutingDataSource();
+        routingDataSource.setDefaultTargetDataSource(master);
+        routingDataSource.setTargetDataSources(dataSources);
+        routingDataSource.afterPropertiesSet();
+        return routingDataSource;
+    }
+
+    @Bean
+    @Primary
+    @DependsOn("routingDataSource")
+    public DataSource dataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+}
---
+++ backend/core/src/main/java/com/onair/hearit/core/infrastructure/datasource/DataSourceType.java
@@ -0,0 +1,6 @@
+package com.onair.hearit.core.infrastructure.datasource;
+
+public enum DataSourceType {
+    MASTER,
+    SLAVE;
+}
