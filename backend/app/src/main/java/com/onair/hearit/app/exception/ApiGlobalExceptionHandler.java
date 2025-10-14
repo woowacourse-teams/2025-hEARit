ce00b02855b1819363a49a39b0ecd7bb994c40fe
YuyoungRhee <bbclara@naver.com>
2025-10-14 21:26:19 +0900

    [BE] feat: ApiGlobalExceptionHandler에 HttpMessageNotReadableException handle 추가

--- backend/app/src/main/java/com/onair/hearit/app/exception/ApiGlobalExceptionHandler.java
+++ backend/app/src/main/java/com/onair/hearit/app/exception/ApiGlobalExceptionHandler.java
@@ -66,8 +66,7 @@ public class ApiGlobalExceptionHandler {
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ProblemDetail handleHttpMessageNotReadableException(HttpMessageNotReadableException ex,
-                                                               HttpServletRequest request) {
+    public ProblemDetail handleHttpMessageNotReadableException(HttpMessageNotReadableException ex, HttpServletRequest request) {
         return buildProblemDetail(ErrorCode.INVALID_INPUT, "요청 본문을 읽을 수 없습니다", request);
     }
 
