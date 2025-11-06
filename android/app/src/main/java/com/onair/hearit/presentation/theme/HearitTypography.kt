8c13d1c065d6d31b2d16119c79578113c3fb9200
Ham BeomJoon <hbj0802@naver.com>
2025-11-06 13:42:28 +0900

    [AN] feat: HearitTypography 추가 #716
    *   [AN] feat: Pretendard 폰트를 사용한 HearitTypography 정의
    *   [AN] refactor: Pretendard FontFamily 이름 변경

---
+++ android/app/src/main/java/com/onair/hearit/presentation/theme/HearitTypography.kt
@@ -0,0 +1,77 @@
+package com.onair.hearit.presentation.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val HearitTypoGraphy =
+    Typography(
+        // Headline: 중간 제목
+        headlineLarge =
+            TextStyle(
+                fontFamily = PretendardFontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 32.sp,
+                lineHeight = 40.sp,
+            ),
+        headlineMedium =
+            TextStyle(
+                fontFamily = PretendardFontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 28.sp,
+                lineHeight = 36.sp,
+            ),
+        headlineSmall =
+            TextStyle(
+                fontFamily = PretendardFontFamily,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 24.sp,
+                lineHeight = 32.sp,
+            ),
+        // Body: 본문
+        bodyLarge =
+            TextStyle(
+                fontFamily = PretendardFontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+            ),
+        bodyMedium =
+            TextStyle(
+                // 이걸 Body로 사용
+                fontFamily = PretendardFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+                lineHeight = 20.sp,
+            ),
+        bodySmall =
+            TextStyle(
+                fontFamily = PretendardFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 12.sp,
+                lineHeight = 16.sp,
+            ),
+        // Label: 버튼, 라벨
+        labelLarge =
+            TextStyle(
+                fontFamily = PretendardFontFamily,
+                fontWeight = FontWeight.Medium,
+                fontSize = 14.sp,
+                lineHeight = 20.sp,
+            ),
+        labelMedium =
+            TextStyle(
+                fontFamily = PretendardFontFamily,
+                fontWeight = FontWeight.Medium,
+                fontSize = 12.sp,
+                lineHeight = 16.sp,
+            ),
+        labelSmall =
+            TextStyle(
+                fontFamily = PretendardFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 11.sp,
+                lineHeight = 16.sp,
+            ),
+    )
