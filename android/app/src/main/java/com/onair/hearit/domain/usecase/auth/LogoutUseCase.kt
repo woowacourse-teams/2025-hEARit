fe61294f1f59c3c9fe0315a947e95324ff46ccaa
Ham BeomJoon <hbj0802@naver.com>
2025-11-11 13:20:46 +0900

    [AN] fix: 카카오 로그아웃 실패해도 로컬 데이터 정리 #718

--- android/app/src/main/java/com/onair/hearit/domain/usecase/auth/LogoutUseCase.kt
+++ android/app/src/main/java/com/onair/hearit/domain/usecase/auth/LogoutUseCase.kt
@@ -15,15 +15,20 @@ class LogoutUseCase(
 ) {
     suspend operator fun invoke(): Result<Unit> =
         runCatching {
-            suspendCoroutine { cont ->
-                kakaoClient.logout { error ->
-                    if (error != null) {
-                        Timber.e(error, "카카오 로그아웃 실패")
-                        cont.resumeWithException(error)
-                    } else {
-                        cont.resume(Unit)
+            runCatching {
+                suspendCoroutine { cont ->
+                    kakaoClient.logout { error ->
+                        if (error != null) {
+                            Timber.e(error, "카카오 로그아웃 실패")
+                            cont.resumeWithException(error)
+                        } else {
+                            Timber.d("카카오 로그아웃 성공")
+                            cont.resume(Unit)
+                        }
                     }
                 }
+            }.onFailure {
+                Timber.w(it, "카카오 로그아웃 실패했지만 로컬 데이터는 정리합니다")
             }
 
             authRepository
