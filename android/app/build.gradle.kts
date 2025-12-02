79a7632ff8afb71211a3d67baf93f19b99a6c8a2
Ham BeomJoon <hbj0802@naver.com>
2025-12-02 13:00:23 +0900

    [AN] build: v1.3.4 업그레이드

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.onair.hearit"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10303
-        versionName = "1.3.3"
+        versionCode = 10304
+        versionName = "1.3.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         manifestPlaceholders += mapOf()
