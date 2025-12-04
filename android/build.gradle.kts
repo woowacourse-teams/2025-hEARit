a5b7be5eee232fb7da24afc1ea1fec92bb425a42
Ham BeomJoon <hbj0802@naver.com>
2025-12-04 12:44:22 +0900

    [AN] chore: Room 컴파일러 KAPT에서 KSP로 마이그레이션 #739
    *   [AN] chore: 루트 build.gradle.kts에 KSP 플러그인 버전 정의 추가
    *   [AN] chore: 앱 모듈에서 kapt 플러그인 제거 및 ksp 플러그인 적용
    *   [AN] chore: Room 컴파일러 의존성 kapt에서 ksp로 변경

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,4 +8,5 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "13.0.0" apply false
     id("com.google.firebase.crashlytics") version "3.0.5" apply false
     id("com.google.android.gms.oss-licenses-plugin") version "0.10.7" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
 }
