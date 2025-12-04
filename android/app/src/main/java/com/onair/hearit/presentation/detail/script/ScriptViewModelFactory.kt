a7665b2860d94bf8ef2b5735e4b7030be7632129
rosemin928 <rosemin928@naver.com>
2025-12-04 19:01:51 +0900

    [AN] refactor: ViewModelFactory 제거 #740

--- android/app/src/main/java/com/onair/hearit/presentation/detail/script/ScriptViewModelFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.onair.hearit.presentation.detail.script
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-
-@Suppress("UNCHECKED_CAST")
-class ScriptViewModelFactory : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T = ScriptViewModel() as T
-}
