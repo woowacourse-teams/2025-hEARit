13678bf49fce31507ec229f85685b08562fab94d
Ham BeomJoon <hbj0802@naver.com>
2025-12-04 13:33:46 +0900

    [AN] refactor: NetworkResult 확장 함수 로직 및 네이밍 변경 #742
    *   [AN] refactor: Result<NetworkResult<T>> 수신 객체 타입을 NetworkResult<T>로 변경
    *   [AN] refactor: mapListOrThrowDomain -> toDomainResultList로 네이밍 변경 및 구현 단순화
    *   [AN] refactor: mapOrThrowDomain -> toDomainResult로 네이밍 변경 및 구현 단순화
    *   [AN] feat: 변환 로직이 없는 toDomainResult 확장 함수 추가

--- android/app/src/main/java/com/onair/hearit/data/repository/ResultDomainMapper.kt
+++ android/app/src/main/java/com/onair/hearit/data/repository/ResultDomainMapper.kt
@@ -3,13 +3,20 @@ package com.onair.hearit.data.repository
 import com.onair.hearit.data.datasource.NetworkResult
 import com.onair.hearit.domain.DomainExceptionMapper.toDomainException
 
-inline fun <T, R> Result<NetworkResult<List<T>>>.mapListOrThrowDomain(crossinline transform: (T) -> R): Result<List<R>> =
-    mapOrThrowDomain { list -> list.map { transform(it) } }
+inline fun <T, R> NetworkResult<List<T>>.toDomainResultList(crossinline transform: (T) -> R): Result<List<R>> =
+    when (this) {
+        is NetworkResult.Success -> Result.success(data.map { transform(it) })
+        is NetworkResult.Failure -> Result.failure(toDomainException(this))
+    }
 
-inline fun <T, R> Result<NetworkResult<T>>.mapOrThrowDomain(transform: (T) -> R): Result<R> =
-    mapCatching { result ->
-        when (result) {
-            is NetworkResult.Success -> transform(result.data)
-            is NetworkResult.Failure -> throw toDomainException(result)
-        }
+inline fun <T, R> NetworkResult<T>.toDomainResult(transform: (T) -> R): Result<R> =
+    when (this) {
+        is NetworkResult.Success -> Result.success(transform(data))
+        is NetworkResult.Failure -> Result.failure(toDomainException(this))
+    }
+
+fun <T> NetworkResult<T>.toDomainResult(): Result<T> =
+    when (this) {
+        is NetworkResult.Success -> Result.success(data)
+        is NetworkResult.Failure -> Result.failure(toDomainException(this))
     }
