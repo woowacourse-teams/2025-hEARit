c2169624bfcbfd17e5960d4da85f29ce0dd7ccf3
rosemin928 <rosemin928@naver.com>
2025-12-07 18:20:45 +0900

    [AN] refactor: 주석 처리된 카테고리 선택 시 화면 전환 로직 제거 #740

--- android/app/src/main/java/com/onair/hearit/presentation/detail/PlayerDetailActivity.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/detail/PlayerDetailActivity.kt
@@ -528,13 +528,6 @@ class PlayerDetailActivity :
             AnalyticsEventNames.DETAIL_CATEGORY_SELECTED,
             mapOf(AnalyticsParamKeys.CATEGORY_NAME to name),
         )
-//        val input = SearchInput.Category(id, name, colorCode)
-//        val resultIntent =
-//            Intent().apply {
-//                putExtras(input.toBundle())
-//            }
-//        setResult(RESULT_OK, resultIntent)
-//        finish()
     }
 
     override fun onClickSource(
