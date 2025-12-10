1f5b03fdb812d2971e53be009cd826c60883bb0d
rosemin928 <rosemin928@naver.com>
2025-12-10 20:32:42 +0900

    [AN] refactor: SettingFragment의 ViewBinding 제거 #740

--- android/app/src/main/java/com/onair/hearit/presentation/setting/SettingFragment.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/setting/SettingFragment.kt
@@ -11,19 +11,15 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.compose.rememberNavController
+import com.onair.hearit.analytics.AnalyticsLogger
 import com.onair.hearit.presentation.login.LoginActivity
 import com.onair.hearit.presentation.main.MainViewModel
-import com.onair.hearit.analytics.AnalyticsLogger
-import com.onair.hearit.databinding.FragmentSettingBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class SettingFragment : Fragment() {
     private val mainViewModel: MainViewModel by activityViewModels()
-    @Suppress("ktlint:standard:backing-property-naming")
-    private var _binding: FragmentSettingBinding? = null
-    private val binding get() = _binding!!
     private val viewModel: SettingViewModel by viewModels()
 
     @Inject
