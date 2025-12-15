ecfa762eef5cf8773bbb238b5b265ce246f1e32d
rosemin928 <rosemin928@naver.com>
2025-12-15 17:40:32 +0900

    [AN] refactor: 스크립트 중앙 정렬하도록 수정 #749

--- android/app/src/main/java/com/onair/hearit/presentation/detail/component/DetailScriptRow.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/detail/component/DetailScriptRow.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.onair.hearit.domain.model.ScriptLine
@@ -45,6 +46,7 @@ fun DetailScriptRow(
                 fontSize = animatedFontSize.sp,
                 fontWeight = if (isHighlighted) FontWeight.Bold else FontWeight.Medium,
             ),
+        textAlign = TextAlign.Center,
         modifier =
             modifier
                 .fillMaxWidth()
