835e88a9e08e547de30c2f01649d69014003a23a
Ham BeomJoon <hbj0802@naver.com>
2025-12-16 20:52:15 +0900

    [AN] refactor: ProfileContent 마진 조절 #766

--- android/app/src/main/java/com/onair/hearit/presentation/setting/component/ProfileContent.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/setting/component/ProfileContent.kt
@@ -65,7 +65,7 @@ fun ProfileContent(
 
         BottomInfo(appVersion = appVersion)
 
-        Spacer(modifier = Modifier.height(68.dp))
+        Spacer(modifier = Modifier.height(60.dp))
     }
 }
 
@@ -83,10 +83,12 @@ private fun NicknameCard(
                     width = 2.dp,
                     color = HearitPurple1,
                     shape = shape,
-                ).background(
+                )
+                .background(
                     color = HearitBlack,
                     shape = shape,
-                ).padding(horizontal = 60.dp, vertical = 12.dp),
+                )
+                .padding(horizontal = 60.dp, vertical = 12.dp),
         contentAlignment = Alignment.Center,
     ) {
         Text(
@@ -113,7 +115,7 @@ private fun BottomInfo(
             style = HearitTypoGraphy.bodyMedium,
         )
 
-        Spacer(modifier = Modifier.height(12.dp))
+        Spacer(modifier = Modifier.height(8.dp))
 
         Text(
             text = stringResource(R.string.all_hearit_email),
