cfaeefac0f5c1d3b7c39de196747e438cd1ad763
[CLOUD4] 백승주 <bsj5967@naver.com>
2025-12-26 04:05:46 +0900

    [BE] chore: 알맞은 패키치 위치로 이동 #763

--- backend/app/src/main/java/com/onair/hearit/app/playinghistory/infrastructure/buffer/RedisConfig.java
+++ backend/app/src/main/java/com/onair/hearit/app/playinghistory/infrastructure/buffer/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.onair.hearit.app.playinghistory.infrastructure.scheduler;
+package com.onair.hearit.app.playinghistory.infrastructure.buffer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
