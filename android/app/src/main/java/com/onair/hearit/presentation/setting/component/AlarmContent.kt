6dede377629676074cae84366b9124694e684024
rosemin928 <rosemin928@naver.com>
2025-12-25 17:42:06 +0900

    [AN] chore: 불필요한 import 제거 #750

--- android/app/src/main/java/com/onair/hearit/presentation/setting/component/AlarmContent.kt
+++ android/app/src/main/java/com/onair/hearit/presentation/setting/component/AlarmContent.kt
@@ -1,11 +1,5 @@
 package com.onair.hearit.presentation.setting.component
 
-import android.Manifest
-import android.content.Context
-import android.content.pm.PackageManager
-import android.os.Build
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -19,19 +13,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import com.onair.hearit.R
-import com.onair.hearit.presentation.setting.SettingViewModel
 import com.onair.hearit.presentation.theme.HearitBlack
 
 @Composable
