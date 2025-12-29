a0eb02e0ec0f1072314ea441c3026948ee83b9cf
byesol <bsj5967@naver.com>
2025-12-29 16:46:48 +0900

    [BE] refactor: Bookmark 엔티티 및 로직을 UUID 기반으로 수정 #779

--- backend/app/src/main/java/com/onair/hearit/app/explore/application/scorefactor/BookmarkScoreFactor.java
+++ backend/app/src/main/java/com/onair/hearit/app/explore/application/scorefactor/BookmarkScoreFactor.java
@@ -9,6 +9,7 @@ import com.onair.hearit.core.infrastructure.jpa.CategoryBookmarkCount;
 import com.onair.hearit.core.infrastructure.jpa.MemberRepository;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -45,7 +46,7 @@ public class BookmarkScoreFactor implements ScoreFactor {
     @Override
     public Map<Long, Double> calculate(String uuid, List<Hearit> hearits) {
         Member member = getMemberByUuid(uuid);
-        Map<Long, Long> bookmarkCountsByCategory = getBookmarkCountsByCategory(member.getId());
+        Map<Long, Long> bookmarkCountsByCategory = getBookmarkCountsByCategory(member.getUuid());
         long totalBookmarkCount = calculateTotalBookmarkCount(bookmarkCountsByCategory);
         return hearits.stream()
                 .collect(Collectors.toMap(
@@ -55,12 +56,12 @@ public class BookmarkScoreFactor implements ScoreFactor {
     }
 
     private Member getMemberByUuid(String uuid) {
-        return memberRepository.findByUuid(uuid)
+        return memberRepository.findByUuid(UUID.fromString(uuid))
                 .orElseThrow(() -> new NotFoundException("uuid", uuid));
     }
 
-    private Map<Long, Long> getBookmarkCountsByCategory(Long memberId) {
-        return bookmarkRepository.countMemberBookmarksByCategoryId(memberId).stream()
+    private Map<Long, Long> getBookmarkCountsByCategory(UUID memberUuid) {
+        return bookmarkRepository.countMemberBookmarksByCategoryId(memberUuid).stream()
                 .collect(Collectors.toMap(
                         CategoryBookmarkCount::getCategoryId,
                         CategoryBookmarkCount::getCount
