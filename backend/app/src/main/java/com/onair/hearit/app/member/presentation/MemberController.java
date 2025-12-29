0b36be1e25bbb9cb671660f38a8203fdb494ad98
byesol <bsj5967@naver.com>
2025-12-29 17:03:13 +0900

    [BE] refactor: Member 관련 로직을 UUID 기반으로 변경 #779
    - `memberId`를 `UUID`로 변경하며 관련 코드 전체 수정
    - `Member.uuid`의 컬럼 타입을 `BINARY(16)`으로 변경
    - `MemberRepository` 메서드 파라미터 및 반환 타입을 `UUID`로 수정
    - 서비스 및 컨트롤러에서 `UUID`를 사용하도록 로직 개선
    - 불필요해진 `memberId` 관련 메서드 및 코드 제거

--- backend/app/src/main/java/com/onair/hearit/app/member/presentation/MemberController.java
+++ backend/app/src/main/java/com/onair/hearit/app/member/presentation/MemberController.java
@@ -21,7 +21,7 @@ public class MemberController {
     @GetMapping("/me")
     public ResponseEntity<MemberInfoResponse> readCurrentMemberInfo(@AuthenticationPrincipal RequestUser requestUser) {
         UserInfo userInfo = requestUser.getUserInfo();
-        MemberInfoResponse response = memberService.getMember(userInfo.getMemberId());
+        MemberInfoResponse response = memberService.getMember(userInfo.getMemberUuid());
         return ResponseEntity.ok(response);
     }
 }
