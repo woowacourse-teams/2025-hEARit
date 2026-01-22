7795541a68f0f45078365f5f143c3da331c6f24f
YuyoungRhee <bbclara@naver.com>
2026-01-22 10:43:04 +0900

    [BE] refactor(playinghistory): config 패키지 위치 수정 #808

--- backend/app/src/main/java/com/onair/hearit/app/playinghistory/infrastructure/config/RedisConfig.java
+++ backend/app/src/main/java/com/onair/hearit/app/playinghistory/infrastructure/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.onair.hearit.app.playinghistory.infrastructure.buffer;
+package com.onair.hearit.app.playinghistory.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- backend/app/src/main/java/com/onair/hearit/app/playinghistory/infrastructure/config/RedissonConfig.java
+++ backend/app/src/main/java/com/onair/hearit/app/playinghistory/infrastructure/config/RedissonConfig.java
@@ -1,4 +1,4 @@
-package com.onair.hearit.app.playinghistory.infrastructure.buffer;
+package com.onair.hearit.app.playinghistory.infrastructure.config;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
