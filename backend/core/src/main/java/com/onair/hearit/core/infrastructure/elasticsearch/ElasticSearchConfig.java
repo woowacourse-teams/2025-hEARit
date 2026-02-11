730f2ce780de52493d5881aa7e03df0e953bd537
Gabin Choi <choegabin171@gmail.com>
2026-02-12 02:06:32 +0900

    [BE] feat: Elasticsearch 설정 및 HearitDocument 추가 #802

---
+++ backend/core/src/main/java/com/onair/hearit/core/infrastructure/elasticsearch/ElasticSearchConfig.java
@@ -0,0 +1,32 @@
+package com.onair.hearit.core.infrastructure.elasticsearch;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.onair.hearit.core.infrastructure.elasticsearch.repository")
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    private final String uris;
+    private final String username;
+    private final String password;
+
+    public ElasticSearchConfig(@Value("${spring.elasticsearch.uris}") String uris,
+                               @Value("${spring.elasticsearch.username}") String username,
+                               @Value("${spring.elasticsearch.password}") String password) {
+        this.uris = uris;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(uris)
+                .withBasicAuth(username, password)
+                .build();
+    }
+}
