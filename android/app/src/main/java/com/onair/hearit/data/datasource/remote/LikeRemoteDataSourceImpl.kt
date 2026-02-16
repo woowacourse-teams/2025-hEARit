38d322feff82c4b0e1304a04d3a35069c1b46c36
rosemin928 <rosemin928@naver.com>
2026-02-16 21:50:15 +0900

    [AN] refactor: LikeRemoteDataSource에서 handleApiCall을 handleApiCallUnit으로 변경 #824

--- android/app/src/main/java/com/onair/hearit/data/datasource/remote/LikeRemoteDataSourceImpl.kt
+++ android/app/src/main/java/com/onair/hearit/data/datasource/remote/LikeRemoteDataSourceImpl.kt
@@ -3,7 +3,7 @@ package com.onair.hearit.data.datasource.remote
 import com.onair.hearit.data.api.LikeService
 import com.onair.hearit.data.datasource.ErrorResponseHandler
 import com.onair.hearit.data.datasource.NetworkResult
-import com.onair.hearit.data.datasource.handleApiCall
+import com.onair.hearit.data.datasource.handleApiCallUnit
 import javax.inject.Inject
 
 class LikeRemoteDataSourceImpl @Inject constructor(
@@ -11,13 +11,13 @@ class LikeRemoteDataSourceImpl @Inject constructor(
     private val errorResponseHandler: ErrorResponseHandler,
 ) : LikeRemoteDataSource {
     override suspend fun addLike(hearitId: Long): NetworkResult<Unit> =
-        handleApiCall(
+        handleApiCallUnit(
             apiCall = { likeService.postLike(hearitId) },
             errorHandler = errorResponseHandler,
         )
 
     override suspend fun deleteLike(hearitId: Long): NetworkResult<Unit> =
-        handleApiCall(
+        handleApiCallUnit(
             apiCall = { likeService.deleteLike(hearitId) },
             errorHandler = errorResponseHandler,
         )
